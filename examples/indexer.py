b2d3dfe396d25a4c9a39d478ed95edd219e3a2f7
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-10-29 13:38:29 -0400

    adds indexer example

--- examples/indexer.py
+++ examples/indexer.py
@@ -8,5 +8,8 @@ from yass import Indexer
 indexer = Indexer('path/to/data.bin', n_channels=50,
                   mode='long', dtype='float64')
 
-# read some data
-indexer.read()
+# read observations from 1000 to 2000 in channels 1 and 5
+indexer.read(observations=(1000, 2000), channels=(1, 5))
+
+# read observations from 1000 to 2000 in all channels
+indexer.read(observations=(1000, 2000), channels='all')
