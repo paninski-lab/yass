49519f4470e99bfd3ef8a5c6d38e2380c092d38b
Peter Lee <jinhyungp.lee@gmail.com>
2017-11-15 15:04:43 -0500

    new neural net modules

---
+++ src/yass/neuralnetwork/detect.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+import pkg_resources
+import numpy as np
+
+from .nndetector import NeuralNetDetector
+from .nntriage import NeuralNetTriage
+from .remove import remove_duplicate_spikes_by_energy
+from .score import get_score
+
+from ..geometry import order_channels_by_distance
+
+def get_spikes(X, T_batch, buff, neighChannels, geom, 
+               n_features, temporal_window, th_triage, 
+               nnd, nnt, path_to_nndfile, path_to_aefile, 
+               path_to_nntfile):
+
+    T, C = X.shape
+    
+    T_small = np.min((T_batch,T))
+    nbatches = int(np.ceil(float(T)/T_small))
+    if nbatches == 1:
+        buff = 0
+
+    # neighboring channel info
+    nneigh = np.max(np.sum(neighChannels, 0))
+    c_idx = np.ones((C, nneigh), 'int32')*C
+    for c in range(C):
+        ch_idx, temp = order_channels_by_distance(c,np.where(neighChannels[c])[0],geom)
+        c_idx[c,:ch_idx.shape[0]] = ch_idx
+
+
+    # input 
+    x_tf = tf.placeholder("float", [T_small+2*buff, C])
+
+    local_max_idx_tf = nnd.get_spikes(x_tf, T_small+2*buff, nneigh, c_idx, temporal_window)
+    score_train_tf = nnd.get_score_train(x_tf)
+    energy_tf = tf.reduce_sum(tf.square(score_train_tf),axis=2)
+    energy_val_tf = tf.gather_nd(energy_tf, local_max_idx_tf)
+    triage_prob_tf = nnt.triage_prob(x_tf, T_small+2*buff, nneigh, c_idx)
+    result = (local_max_idx_tf, score_train_tf, energy_val_tf, triage_prob_tf)
+        
+    energy_train_tf = tf.placeholder("float", [T_small+2*buff, C])
+    spike_index_tf = remove_duplicate_spikes_by_energy(energy_train_tf, T_small+2*buff, c_idx, temporal_window)        
+    
+    score_train_placeholder = tf.placeholder("float", [T_small+2*buff, C, n_features])
+    spike_index_clear_tf = tf.placeholder("int64", [None, 2])
+    score_tf = get_score(score_train_placeholder, spike_index_clear_tf, T_small+2*buff, n_features, c_idx)
+    
+    spike_index_clear_all = np.zeros((10000000, 2), 'int32')
+    spike_index_collision_all = np.zeros((10000000, 2), 'int32')
+    score_all = np.zeros((10000000, n_features, nneigh))
+
+    with tf.Session() as sess:
+
+        nnd.saver.restore(sess, path_to_nndfile)
+        nnd.saver_ae.restore(sess, path_to_aefile)
+        nnt.saver.restore(sess, path_to_nntfile)
+
+        counter_clear = 0
+        counter_collision = 0
+        for j in range(nbatches):
+            if buff == 0:
+                X_batch = X
+
+                T_min = 0
+                T_max = T_small
+                t_add = 0
+
+            elif j == 0:
+                X_batch = X[:(T_small+2*buff)]
+
+                T_min = 0
+                T_max = T_small
+                t_add = 0
+
+            elif (T_small*(j+1)+buff) > T:
+                X_temp = X[(T_small*j-buff):]
+                zeros_size = T_small+2*buff - X_temp.shape[0]
+                Zerobuff = np.zeros((zeros_size, X_temp.shape[1]))
+                X_batch = np.concatenate((X_temp, Zerobuff))
+
+                T_min = buff
+                T_max = X_temp.shape[1]
+                t_add = T_small*j - buff
+
+            else:
+                X_batch = X[(T_small*j-buff):(T_small*(j+1)+buff)]
+
+                T_min = buff
+                T_max = buff+T_small
+                t_add = T_small*j - buff
+
+            local_max_idx, score_train, energy_val, triage_prob  = sess.run(result, feed_dict={x_tf: X[:(T_small+2*buff)]})
+
+            energy_train = np.zeros((T_small+2*buff,C))
+            energy_train[local_max_idx[:,0],local_max_idx[:,1]] = energy_val
+            spike_index = sess.run(spike_index_tf, feed_dict={energy_train_tf: energy_train})
+            spike_index = spike_index[np.logical_and(
+                        spike_index[:, 0] >= T_min, spike_index[:, 0] < T_max)]
+
+
+            idx_clean = triage_prob[spike_index[:,0],spike_index[:,1]] > th_triage
+
+            spike_index_clear = spike_index[idx_clean]
+            spike_index_collision = spike_index[~idx_clean]
+
+            score = sess.run(score_tf, feed_dict={score_train_placeholder:score_train, spike_index_clear_tf:spike_index_clear})
+            
+            spike_index_clear[:,0] = spike_index_clear[:,0] + t_add
+            spike_index_collision[:,0] = spike_index_collision[:,0] + t_add
+
+            n_clean = spike_index_clear.shape[0]
+            n_col = spike_index_collision.shape[0]
+
+            spike_index_clear_all[counter_clear:(counter_clear+n_clean)] = spike_index_clear
+            spike_index_collision_all[counter_collision:(counter_collision+n_col)] = spike_index_collision
+            score_all[counter_clear:(counter_clear+n_clean)] = score
+            
+            counter_clear += n_clean
+            counter_collision += n_col
+
+    spike_index_clear_all = spike_index_clear_all[:counter_clear]
+    spike_index_collision_all = spike_index_collision_all[:counter_collision]
+    score_all = score_all[:counter_clear]
+    
+    return spike_index_clear_all, spike_index_collision_all, score_all
---
+++ src/yass/neuralnetwork/train_triage.py
@@ -0,0 +1,87 @@
+import os
+import numpy as np
+import tensorflow as tf
+import progressbar
+
+from .utils import weight_variable, bias_variable, conv2d, conv2d_VALID
+
+def train_triage(x_train, y_train, n_filters, n_iter, n_batch, l2_reg_scale, train_step_size, nn_name):
+    """
+        Trains the triage network
+
+        Parameters:
+        -----------
+        x_train: np.array
+            [number of data, temporal length, number of channels] training data for the triage network.
+        y_train: np.array
+            [number of data] training label for the triage network.
+        nn_name: string
+            name of the .ckpt to be saved.            
+    """                
+    # get parameters
+    ndata, R, C = x_train.shape
+    K1, K2 = n_filters
+
+    # x and y input tensors
+    x_tf = tf.placeholder("float", [n_batch, R, C])
+    y_tf = tf.placeholder("float", [n_batch])
+    
+    # first layer: temporal feature
+    W1 = weight_variable([R, 1, 1, K1])
+    b1 = bias_variable([K1])
+    layer1 = tf.nn.relu(conv2d_VALID(
+        tf.expand_dims(x_tf, -1), W1) + b1)
+    
+    # second layer: feataure mapping
+    W11 = weight_variable([1, 1, K1, K2])
+    b11 = bias_variable([K2])
+    layer11 = tf.nn.relu(conv2d(layer1, W11) + b11)
+    
+    # third layer: spatial convolution
+    W2 = weight_variable([1,C,K2,1])
+    b2 = bias_variable([1])
+    o_layer = tf.squeeze(conv2d_VALID(layer11, W2) + b2)
+
+    # cross entropy
+    cross_entropy = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+            logits=o_layer, labels=y_tf) )
+
+    # regularization term
+    weights = tf.trainable_variables()
+    l2_regularizer = tf.contrib.layers.l2_regularizer(
+        scale= l2_reg_scale)
+    regularization_penalty = tf.contrib.layers.apply_regularization(
+        l2_regularizer, weights)
+    regularized_loss = cross_entropy + regularization_penalty
+
+    # train step
+    train_step = tf.train.AdamOptimizer(
+        train_step_size).minimize(regularized_loss)
+
+    # saver
+    saver = tf.train.Saver({"W1": W1, "W11": W11, "W2": W2, "b1": b1, "b11": b11, "b2": b2})
+    
+    ############
+    # training #
+    ############
+    
+    bar = progressbar.ProgressBar(maxval=n_iter)
+    with tf.Session() as sess:
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+
+        for i in range(0, n_iter):
+            idx_batch = np.random.choice(ndata, n_batch, replace=False)
+            sess.run(train_step, feed_dict={x_tf: x_train[idx_batch], y_tf: y_train[idx_batch]})
+            bar.update(i+1)
+        saver.save(sess, nn_name)
+
+        idx_batch = np.random.choice(ndata, n_batch, replace=False)
+        output = sess.run(o_layer, feed_dict={x_tf: x_train[idx_batch]})
+        y_test = y_train[idx_batch]
+        tp = np.mean(output[y_test==1] > 0)
+        fp = np.mean(output[y_test==0] > 0)
+        
+        print('Approximate training true positive rate: '+str(tp)+', false positive rate: '+str(fp))
+    bar.finish()
