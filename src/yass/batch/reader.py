ba3f12e3afa3df22c3bd90c36066f069f45284f5
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-11-21 10:36:25 -0500

    improves recs reader

--- src/yass/batch/reader.py
+++ src/yass/batch/reader.py
@@ -6,9 +6,9 @@ import numpy as np
 class RecordingsReader(object):
     """
     Neural recordings reader, supports wide and long data. If a file with the
-    same name but yaml extension exists in the directory it looks for
-    dtype, channels and data_format, otherwise you need to pass the parameters
-    in the constructor
+    same name but yaml extension exists in the directory it looks for dtype,
+    channels and data_format, otherwise you need to pass the parameters in the
+    constructor.
 
     Parameters
     ----------
@@ -34,6 +34,13 @@ class RecordingsReader(object):
     ValueError
         If dimensions do not match according to the file size, dtype and
         number of channels
+
+    Notes
+    -----
+    This is just an utility class to index binary files in a consistent way,
+    it does not matter the shape of the file ('wide' or 'long'), indexing is
+    performed in [observations, channels] format. This class is mainly used
+    by other internal YASS classes to maintain a consistent indexing order.
 
     Examples
     --------
@@ -63,42 +70,67 @@ class RecordingsReader(object):
             data_format = params['data_format']
 
         loader = np.memmap if mmap else np.fromfile
-        self.data = loader(path_to_recordings, dtype=dtype)
-        self.data_format = data_format
+        self._data = loader(path_to_recordings, dtype=dtype)
+        self._data_format = data_format
         self._n_channels = n_channels
+        self._dtype = dtype
 
-        if len(self.data) % n_channels:
+        if len(self._data) % n_channels:
             raise ValueError('Wrong dimensions, length of the data does not '
                              'match number of n_channels (observations % '
                              'n_channels != 0, verify that the number of '
                              'n_channels and/or the dtype are correct')
 
-        self._n_observations = int(len(self.data)/n_channels)
+        self._n_observations = int(len(self._data)/n_channels)
 
         dim = ((n_channels, self._n_observations) if data_format == 'wide' else
                (self._n_observations, n_channels))
 
-        self.data = self.data.reshape(dim)
+        self._data = self._data.reshape(dim)
 
     def __getitem__(self, key):
-        key = key if self.data_format == 'long' else key[::-1]
-        return self.data[key]
+        key = key if self._data_format == 'long' else key[::-1]
+        return self._data[key]
 
     def __repr__(self):
         return ('Reader for recordings with {:,} observations and {:,} '
                 'channels in "{}" format'
                 .format(self.observations, self.channels,
-                        self.data_format))
+                        self._data_format))
 
     @property
     def shape(self):
-        return (self.data.shape if self.data_format == 'long' else
-                self.data.shape[::-1])
+        """Data shape in (observations, channels) format
+        """
+        return (self._data.shape if self._data_format == 'long' else
+                self._data.shape[::-1])
 
     @property
     def observations(self):
+        """Number of observations
+        """
         return self._n_observations
 
     @property
     def channels(self):
+        """Number of channels
+        """
         return self._n_channels
+
+    @property
+    def data_format(self):
+        """Data format
+        """
+        return self._data_format
+
+    @property
+    def dtype(self):
+        """Numpy's dtype
+        """
+        return self._dtype
+
+    @property
+    def data(self):
+        """Underlying numpy data
+        """
+        return self._data
