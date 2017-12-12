6875e22d1445eca98c044c5e71a186862a3b61b7
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-12-12 12:19:39 -0500

    adds test to check example, marked as xfail

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,27 @@
 """
 Testing example in README.md
 """
+import os
+
+import pytest
+
 from yass import command_line as cli
 
-def test_example_works():
-    pass
+
+@pytest.fixture
+def path_to_config_sample():
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        'config_sample.yaml')
+    return path
+
+
+@pytest.fixture
+def path_to_output():
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        'data/tmp/output.csv')
+    return path
+
+
+@pytest.mark.xfail(reason='bug has not been fixed')
+def test_example_works(path_to_config_sample, path_to_output):
+    cli._run_pipeline(path_to_config_sample, path_to_output)
