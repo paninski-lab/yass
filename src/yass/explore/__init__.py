72a536007f8afc6361fa91ab57e93fb4e2f731fa
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-12-13 11:39:54 -0500

    adds matplotlib to requirements.txt

--- src/yass/explore/__init__.py
+++ src/yass/explore/__init__.py
@@ -1,3 +1,5 @@
 from .explorers import SpikeTrainExplorer, RecordingExplorer
 
 __all__ = ['SpikeTrainExplorer', 'RecordingExplorer']
+
+# TODO: check if matplotlib is installed, if not, show error message
