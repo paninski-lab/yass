e0e578cef7646b48193fa194a214372fe1c36e5a
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-01-03 18:04:30 -0600

    adds logger in training script

--- src/yass/neuralnetwork/train_all.py
+++ src/yass/neuralnetwork/train_all.py
@@ -1,5 +1,6 @@
-import yass
-from yass import read_config
+import logging
+
+
 from yass.augment import (make_training_data, save_detect_network_params,
                           save_triage_network_params, save_ae_network_params)
 from yass.neuralnetwork import train_detector, train_ae, train_triage
@@ -15,18 +16,22 @@ def train_neural_networks(CONFIG, spike_train, chosen_templates, min_amp,
     Parameters
     ----------
     """
+    logger = logging.getLogger(__name__)
 
     # generate training data
+    logger.info('Generating training data...')
     (x_detect, y_detect,
      x_triage, y_triage,
      x_ae, y_ae) = make_training_data(CONFIG, spike_train, chosen_templates,
                                       min_amp, nspikes)
 
     # train detector
+    logger.info('Training detector network...')
     train_detector(x_detect, y_detect, n_filters_detect, n_iter, n_batch,
                    l2_reg_scale, train_step_size, detectnet_name)
 
     # save detector model parameters
+    logger.info('Saving detector network parameters...')
     save_detect_network_params(filters=n_filters_detect,
                                size=x_detect.shape[1],
                                n_neighbors=x_detect.shape[2],
@@ -34,10 +39,12 @@ def train_neural_networks(CONFIG, spike_train, chosen_templates, min_amp,
                                                                   'yaml'))
 
     # train triage
+    logger.info('Training triage network...')
     train_triage(x_triage, y_triage, n_filters_triage, n_iter, n_batch,
                  l2_reg_scale, train_step_size, triagenet_name)
 
     # save triage model parameters
+    logger.info('Saving triage network parameters...')
     save_triage_network_params(filters=n_filters_triage,
                                size=x_detect.shape[1],
                                n_neighbors=x_detect.shape[2],
@@ -45,9 +52,11 @@ def train_neural_networks(CONFIG, spike_train, chosen_templates, min_amp,
                                                                   'yaml'))
 
     # train autoencoder
+    logger.info('Training autoencoder network...')
     train_ae(x_ae, y_ae, n_features, n_iter, n_batch, train_step_size, ae_name)
 
     # save autoencoder model parameters
+    logger.info('Saving autoencoder network parameters...')
     save_ae_network_params(n_input=x_ae.shape[1],
                            n_features=n_features,
                            output_path=ae_name.replace('ckpt', 'yaml'))
