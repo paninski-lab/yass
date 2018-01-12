de68e7cfbbe065e6833be0316479f54fd7148b90
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-01-12 13:34:46 -0600

    deconvolution code without batching logic

--- src/yass/deconvolute/deconvolute.py
+++ src/yass/deconvolute/deconvolute.py
@@ -3,7 +3,6 @@
 """
 
 import logging
-import os
 import datetime as dt
 import numpy as np
 
@@ -15,42 +14,17 @@ from ..geometry import n_steps_neigh_channels
 # TODO: remove batching logic from here
 # FIXME: use numpy memmap instead of manual file seek
 class Deconvolution(object):
-    """[Description]
 
-    Parameters
-    ----------
+    def __init__(self, config, templates, spike_index, wrec):
 
-    Returns
-    -------
-    """
-
-    def __init__(self, config, templates, spike_index, filename='wrec.bin'):
         self.config = config
         self.templates = templates
         self.spike_index = spike_index
-        self.path_to_file = os.path.join(
-            self.config.data.root_folder, 'tmp', filename)
-
         self.logger = logging.getLogger(__name__)
 
-    def openWFile(self, opt):
-        self.WFile = open(self.path_to_file, opt)
-
-    def closeWFile(self):
-        self.WFile.close()
-
-    def fullMPMU(self):
+    def fullMPMU(self, wrec):
 
         start_time = dt.datetime.now()
-
-        self.openWFile('rb')
-
-        self.logger.debug('wfile is {} {}'
-                          .format(self.path_to_file,
-                                  os.path.getsize(self.path_to_file)))
-        nBatches = self.config.nBatches
-        flattenedLength = (2*(self.config.batch_size + 2*self.config.BUFF)
-                           * self.config.recordings.n_channels)
 
         neighchan = n_steps_neigh_channels(self.config.neighChannels, steps=3)
         C = self.config.recordings.n_channels
@@ -66,6 +40,7 @@ class Deconvolution(object):
         amps_max = np.max(amps, axis=0)
 
         templatesMask = np.zeros((K, C), 'bool')
+
         for k in range(K):
             templatesMask[k] = amps[:, k] > amps_max[k]*0.5
 
@@ -74,138 +49,103 @@ class Deconvolution(object):
         spiketime_all = np.zeros(0, 'int32')
         assignment_all = np.zeros(0, 'int32')
 
-        for i in range(nBatches):
-            self.logger.info("batch {}/{}".format(i+1, nBatches))
-            self.WFile.seek(flattenedLength*i)
-            wrec = self.WFile.read(flattenedLength)
-            wrec = np.fromstring(wrec, dtype='float64')
-            wrec = np.reshape(wrec, (-1, self.config.recordings.n_channels))
+        for c in range(C):
+            nmax = 1000000
+            ids = np.zeros(nmax, 'int32')
+            sts = np.zeros(nmax, 'int32')
+            ns = np.zeros(nmax, 'int32')
 
-            idx_batch = np.logical_and(self.spike_index[:, 0]
-                                       > self.config.batch_size*i,
-                                       self.spike_index[:, 0]
-                                       < self.config.batch_size*(i+1))
-            spike_index_batch = self.spike_index[idx_batch]
-            spike_index_batch[:, 0] = spike_index_batch[
-                :, 0] - self.config.batch_size*i + self.config.BUFF
+            idx_c = self.spike_index[:, 1] == c
+            nc = np.sum(idx_c)
+            if nc > 0:
+                spt_c = self.spike_index[idx_c, 0]
+                ch_idx = np.where(neighchan[c])[0]
 
-            for c in range(C):
-                nmax = 1000000
-                ids = np.zeros(nmax, 'int32')
-                sts = np.zeros(nmax, 'int32')
-                ns = np.zeros(nmax, 'int32')
+                k_idx = np.where(templatesMask[:, c])[0]
+                tt = self.templates[:, ch_idx][:, :, k_idx]
+                Kc = k_idx.shape[0]
 
-                idx_c = spike_index_batch[:, 1] == c
-                nc = np.sum(idx_c)
-                if nc > 0:
-                    spt_c = spike_index_batch[idx_c, 0]
-                    ch_idx = np.where(neighchan[c])[0]
+                if nc > 0 and Kc > 0:
+                    mu = np.reshape(mu_all[k_idx], [1, 1, Kc])
+                    lam1 = lam/np.square(mu)
 
-                    k_idx = np.where(templatesMask[:, c])[0]
-                    tt = self.templates[:, ch_idx][:, :, k_idx]
-                    Kc = k_idx.shape[0]
+                    wf = np.zeros((nc, 2*(R+shift)+1, ch_idx.shape[0]))
 
-                    if nc > 0 and Kc > 0:
-                        mu = np.reshape(mu_all[k_idx], [1, 1, Kc])
-                        lam1 = lam/np.square(mu)
+                    for j in range(nc):
+                        wf[j] = wrec[
+                            spt_c[j]+np.arange(-(R+shift), R+shift+1)][:, ch_idx]
 
-                        wf = np.zeros((nc, 2*(R+shift)+1, ch_idx.shape[0]))
-                        for j in range(nc):
-                            wf[j] = wrec[
-                                spt_c[j]+np.arange(-(R+shift),
-                                                   R+shift+1)][:, ch_idx]
+                    n = np.arange(nc)
+                    i0 = 0
+                    it = 0
+                    while it < iter_max:
+                        nc = n.shape[0]
+                        wf_projs = np.zeros((nc, 2*(R+shift)+1, nrank, Kc))
+                        for k in range(Kc):
+                            wf_projs[:, :, :, k] = np.reshape(np.matmul(np.reshape(
+                                wf[n], [-1, ch_idx.shape[0]]), U_all[ch_idx, k_idx[k]]), [nc, -1, nrank])
 
-                        n = np.arange(nc)
-                        i0 = 0
-                        it = 0
-                        while it < iter_max:
-                            nc = n.shape[0]
-                            wf_projs = np.zeros((nc, 2*(R+shift)+1, nrank, Kc))
-                            for k in range(Kc):
-                                res = np.reshape(
-                                    np.matmul(
-                                        np.reshape(wf[n],
-                                                   [-1, ch_idx.shape[0]]),
-                                        U_all[ch_idx, k_idx[k]]),
-                                    [nc, -1, nrank])
-                                wf_projs[:, :, :, k] = res
+                        obj = np.zeros((nc, 2*shift+1, Kc))
+                        for j in range(2*shift+1):
+                            obj[:, j, :] = np.sum((wf_projs[:, j:(
+                                j+2*R+1)]*np.transpose(W_all[:, k_idx], [0, 2, 1])[np.newaxis, :]), axis=(1, 2))
 
-                            obj = np.zeros((nc, 2*shift+1, Kc))
-                            for j in range(2*shift+1):
-                                obj[:, j, :] = np.sum((wf_projs[:, j:(
-                                    j+2*R+1)]*np.transpose(W_all[:, k_idx],
-                                                           [0, 2, 1])
-                                    [np.newaxis, :]),
-                                    axis=(1, 2))
+                        Ci = obj+(mu*lam1)
+                        Ci = np.square(Ci)/(1+lam1)
+                        Ci = Ci - lam1*np.square(mu)
 
-                            Ci = obj+(mu*lam1)
-                            Ci = np.square(Ci)/(1+lam1)
-                            Ci = Ci - lam1*np.square(mu)
+                        mX = np.max(Ci, axis=1)
+                        st = np.argmax(Ci, axis=1)
+                        idd = np.argmax(mX, axis=1)
+                        st = st[np.arange(st.shape[0]), idd]
 
-                            mX = np.max(Ci, axis=1)
-                            st = np.argmax(Ci, axis=1)
-                            idd = np.argmax(mX, axis=1)
-                            st = st[np.arange(st.shape[0]), idd]
+                        idx_keep = np.max(mX, axis=1) > Th*Th
+                        st = st[idx_keep]
+                        idd = idd[idx_keep]
+                        n = n[idx_keep]
 
-                            idx_keep = np.max(mX, axis=1) > Th*Th
-                            st = st[idx_keep]
-                            idd = idd[idx_keep]
-                            n = n[idx_keep]
+                        n_detected = np.sum(idx_keep)
+                        if it > 0:
+                            idx_keep2 = np.zeros(n_detected, 'bool')
+                            for j in range(n_detected):
 
-                            n_detected = np.sum(idx_keep)
-                            if it > 0:
-                                idx_keep2 = np.zeros(n_detected, 'bool')
-                                for j in range(n_detected):
-                                    if (np.sum(ids[:i0][ns[:i0] == n[j]]
-                                               == idd[j]) == 0):
-                                        idx_keep2[j] = 1
-                                st = st[idx_keep2]
-                                idd = idd[idx_keep2]
-                                n = n[idx_keep2]
-                                n_detected = np.sum(idx_keep2)
+                                if (np.sum(ids[:i0][ns[:i0] == n[j]] ==
+                                   idd[j]) == 0):
+                                    idx_keep2[j] = 1
+                            st = st[idx_keep2]
+                            idd = idd[idx_keep2]
+                            n = n[idx_keep2]
+                            n_detected = np.sum(idx_keep2)
 
-                            if not st.any():
-                                it = iter_max
-                            else:
-                                sts[i0:(i0+n_detected)] = st
-                                ids[i0:(i0+n_detected)] = idd
-                                ns[i0:(i0+n_detected)] = n
-                                i0 = i0 + n_detected
-                                it += 1
+                        if not st.any():
+                            it = iter_max
+                        else:
+                            sts[i0:(i0+n_detected)] = st
+                            ids[i0:(i0+n_detected)] = idd
+                            ns[i0:(i0+n_detected)] = n
+                            i0 = i0 + n_detected
+                            it += 1
 
-                                if it < iter_max:
-                                    for j in range(st.shape[0]):
-                                        wf[n[j], st[j]:(
-                                            st[j]+2*R+1)] -= tt[:, :, idd[j]]
+                            if it < iter_max:
+                                for j in range(st.shape[0]):
+                                    wf[n[j], st[j]:(
+                                        st[j]+2*R+1)] -= tt[:, :, idd[j]]
 
-                        ids = k_idx[ids[:i0]]
-                        sts = sts[:i0] - shift - 1 + spt_c[ns[:i0]] + \
-                            i*self.config.batch_size - self.config.BUFF
-                        spiketime_all = np.concatenate((spiketime_all, sts))
-                        assignment_all = np.concatenate((assignment_all, ids))
+                    ids = k_idx[ids[:i0]]
+                    sts = sts[:i0] - shift - 1 + spt_c[ns[:i0]]
 
-        self.closeWFile()
+                    spiketime_all = np.concatenate((spiketime_all, sts))
+                    assignment_all = np.concatenate((assignment_all, ids))
 
         current_time = dt.datetime.now()
         self.logger.info("Deconvolution done in {0} seconds.".format(
                          (current_time-start_time).seconds))
 
         return np.concatenate((spiketime_all[:, np.newaxis],
-                               assignment_all[:, np.newaxis]),
-                              axis=1)
+                               assignment_all[:, np.newaxis]), axis=1)
 
 
-# TODO: documentation
-# TODO: comment code, it's not clear what it does
 def decompose_dWU(templates, nrank):
-    """[Description]
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-    """
     R, C, K = templates.shape
     W = np.zeros((R, nrank, K), 'float32')
     U = np.zeros((C, nrank, K), 'float32')
@@ -223,17 +163,7 @@ def decompose_dWU(templates, nrank):
     return W, U, mu
 
 
-# TODO: documentation
-# TODO: comment code, it's not clear what it does
 def get_svds(template, nrank):
-    """[Description]
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-    """
     Wall, S_temp, Uall = np.linalg.svd(template)
     imax = np.argmax(np.abs(Wall[:, 0]))
     ss = np.sign(Wall[imax, 1])
