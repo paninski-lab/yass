205bceb65ea7967460f65fb4b3ca00b97963c479
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-01-19 18:26:07 -0500

    removes old pipeline code

--- src/yass/command_line.py
+++ src/yass/command_line.py
@@ -56,34 +56,6 @@ def _run_pipeline(config, output_file, logger_level=logging.INFO):
     logging.basicConfig(level=getattr(logging, logger_level))
     logger = logging.getLogger(__name__)
 
-    # cfg = yass.Config.from_yaml(config)
-
-    # pp = Preprocessor(cfg)
-    # score, spike_index_clear, spike_index_collision = pp.process()
-
-    # mp = Mainprocessor(cfg, score, spike_index_clear, spike_index_collision)
-    # spikeTrain_clear, spike_index_collision = mp.mainProcess()
-
-    # dc = Deconvolution(cfg, np.transpose(
-    #     mp.templates, [1, 0, 2]), spike_index_collision)
-    # spikeTrain_col = dc.fullMPMU()
-
-    # spikeTrain = np.concatenate((spikeTrain_col, spikeTrain_clear))
-    # idx_sort = np.argsort(spikeTrain[:, 0])
-    # spikeTrain = spikeTrain[idx_sort]
-
-    # idx_keep = np.zeros(spikeTrain.shape[0], 'bool')
-    # for k in range(mp.templates.shape[2]):
-    #     idx_c = np.where(spikeTrain[:, 1] == k)[0]
-    #     idx_keep[idx_c[np.concatenate(
-    #         ([True], np.diff(spikeTrain[idx_c, 0]) > 1))]] = 1
-    # spikeTrain = spikeTrain[idx_keep]
-
-    # path_to_file = os.path.join(cfg.data.root_folder, output_file)
-
-    # np.savetxt(path_to_file, spikeTrain, fmt='%i, %i')
-    # print('Done, spike train saved in: {}'.format(path_to_file))
-
     # set yass configuration parameters
     set_config(config)
     CONFIG = read_config()
