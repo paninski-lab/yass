bf6abb05fa919398d19ca80ce784b54e085962ec
calvinytong <calvinytong@gmail.com>
2018-02-01 23:00:30 -0500

    switch batch to abs imports

--- src/yass/batch/__init__.py
+++ src/yass/batch/__init__.py
@@ -1,8 +1,8 @@
-from .batch import BatchProcessor
-from .reader import RecordingsReader
-from .generator import IndexGenerator
-from .pipeline import PipedTransformation, BatchPipeline
-from .vectorize import vectorize_parameter
+from yass.batch.batch import BatchProcessor
+from yass.batch.reader import RecordingsReader
+from yass.batch.generator import IndexGenerator
+from yass.batch.pipeline import PipedTransformation, BatchPipeline
+from yass.batch.vectorize import vectorize_parameter
 
 __all__ = ['BatchProcessor',
            'RecordingsReader',
