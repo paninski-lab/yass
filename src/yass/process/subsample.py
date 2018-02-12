44746bd681bf7d206f15dbd7570a77f706296e6b
Peter Lee <jinhyungp.lee@gmail.com>
2018-02-12 18:06:07 -0500

    fix a bug in subsample

--- src/yass/process/subsample.py
+++ src/yass/process/subsample.py
@@ -5,14 +5,14 @@ def random_subsample(scores, spike_index, n_sample=1000):
     n_channels = len(scores)
     
     for c in range(n_channels):
-        
         n_data = scores[c].shape[0]
-
-        idx_keep = np.random.choice(n_data,
-                                    np.min((n_sample, n_data)),
-                                    replace=False)
-        scores[c] = scores[c][idx_keep]
-        spike_index[c] = spike_index[c][idx_keep]
+        if n_data > n_sample:
+            
+            idx_keep = np.random.choice(n_data,
+                                        n_sample,
+                                        replace=False)
+            scores[c] = scores[c][idx_keep]
+            spike_index[c] = spike_index[c][idx_keep]
         
     return scores, spike_index
         
