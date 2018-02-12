a2bdd691add4e8610ec4055b67c83ad65f168e34
calvinytong <calvinytong@gmail.com>
2018-02-12 17:15:55 -0500

    fix call to yass

--- integration-test/integration-test.sh
+++ integration-test/integration-test.sh
@@ -5,11 +5,11 @@ set -e
 
 # cd to the script directory to avoid pathing issues
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd $DIR/../examples
 
 # Run Examples
 echo "Running config_sample.yaml example"
-yass sort $DIR/../examples/config_sample.yaml
+yass sort config_sample.yaml
 
 echo "Running config_sample_complete.yaml example"
-yass sort $DIR/../examples/config_sample_complete.yaml
+yass sort config_sample_complete.yaml
