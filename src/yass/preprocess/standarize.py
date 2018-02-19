b56d88681e9a53ffa187fe3e4fe0f3c3aa4fe50f
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-19 13:37:47 -0500

    updates standarize from blocks

--- src/yass/preprocess/standarize.py
+++ src/yass/preprocess/standarize.py
@@ -1,11 +1,108 @@
 """
 Recording standarization
 """
+from yass.batch import BatchProcessor
+from yass.util import check_for_files, ExpandPath, LoadFile
+
 import numpy as np
 
 
-def standarize(rec, sampling_freq=None, sd=None):
+@check_for_files(parameters=['output_filename'],
+                 if_skip=[ExpandPath('output_filename'),
+                          LoadFile('output_filename', 'yaml')])
+def standarize(path_to_data, dtype, n_channels, data_shape,
+               sampling_frequency, max_memory, output_path,
+               output_dtype, output_filename='standarized.bin',
+               if_file_exists='skip'):
+    """
+    Standarize recordings in batches and write results to disk. Standard
+    deviation is estimated using the first batch
+
+    Parameters
+    ----------
+    path_to_data: str
+        Path to recordings in binary format
+
+    dtype: str
+        Recordings dtype
+
+    n_channels: int
+        Number of channels in the recordings
+
+    data_shape: str
+        Data shape, can be either 'long' (observations, channels) or
+        'wide' (channels, observations)
+
+    sampling_frequency: int
+        Recordings sampling frequency in Hz
+
+    max_memory: str
+        Max memory to use in each batch (e.g. 100MB, 1GB)
+
+    output_path: str
+        Where to store the standarized recordings
+
+    output_dtype: str
+        dtype  for standarized data
+
+    output_filename: str, optional
+        Filename for the output data, defaults to whitened.bin
+
+    if_file_exists: str, optional
+        One of 'overwrite', 'abort', 'skip'. If 'overwrite' it replaces the
+        standarized data if it exists, if 'abort' if raise a ValueError
+        exception if the file exists, if 'skip' if skips the operation if the
+        file exists
+
+    Returns
+    -------
+    standarized_path: str
+        Path to standarized recordings
+
+    standarized_params: dict
+        A dictionary with the parameters for the standarized recordings
+        (dtype, n_channels, data_format)
+    """
+    # init batch processor
+    bp = BatchProcessor(path_to_data, dtype, n_channels, data_shape,
+                        max_memory)
+
+    # read a batch from all channels
+    batches = bp.multi_channel()
+    first_batch, _, _ = next(batches)
+
+    # estimate standard deviation using the first batch
+    sd = standard_deviation(first_batch, sampling_frequency)
+
+    # apply transformation
+    (standarized_path,
+     standarized_params) = bp.multi_channel_apply(_standarize,
+                                                  mode='disk',
+                                                  output_path=output_path,
+                                                  if_file_exists='overwrite',
+                                                  cast_dtype=output_dtype,
+                                                  sd=sd)
+
+    return standarized_path, standarized_params
+
+
+def _standarize(rec, sampling_freq=None, sd=None):
     """Standarize recordings
+
+    Parameters
+    ----------
+    sampling_freq: int, optional
+        Recording sample frequency, if None, sd is required
+
+    sd: float, optional
+        Standard deviation, if None, will be calculated
+
+
+    Returns
+    -------
+    numpy.ndarray
+        Standarized recordings
+
     """
     if sd is None:
         sd = standard_deviation(rec, sampling_freq)
@@ -19,6 +116,7 @@ def standard_deviation(rec, sampling_freq):
     Parameters
     ----------
     rec : matrix [length of recording, number of channels]
+
     sampling_freq : int
         the sampling rate (in Hz)
 
