7c01212b97f2d37f63f66a0956d16eb17dbe1836
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-19 20:13:52 -0500

    splits processor in cluster and templates steps

--- src/yass/templates/run.py
+++ src/yass/templates/run.py
@@ -1,3 +1,65 @@
+import os
+import logging
+import datetime
 
-def run():
-    pass
+from yass import read_config
+from yass.process.templates import get_and_merge_templates as gam_templates
+
+
+def run(spike_train, output_directory='tmp/',
+        recordings_filename='standarized.bin'):
+    """(TODO add missing documentation)
+
+
+    Parameters
+    ----------
+    spike_train: ?
+
+    output_directory: str, optional
+        Output directory (relative to CONFIG.data.root_folder) used to load
+        the recordings to generate templates, defaults to tmp/
+
+    recordings_filename: str, optional
+        Recordings filename (relative to CONFIG.data.root_folder/
+        output_directory) used to generate the templates, defaults to
+        whitened.bin
+
+
+    Returns
+    -------
+    templates ?
+
+    Examples
+    --------
+
+    .. literalinclude:: ../examples/templates.py
+    """
+    CONFIG = read_config()
+
+    startTime = datetime.datetime.now()
+
+    Time = {'t': 0, 'c': 0, 'm': 0, 's': 0, 'e': 0}
+
+    logger = logging.getLogger(__name__)
+
+    _b = datetime.datetime.now()
+
+    logger.info("Getting Templates...")
+
+    path_to_recordings = os.path.join(CONFIG.data.root_folder,
+                                      output_directory,
+                                      recordings_filename)
+    merge_threshold = CONFIG.templates.merge_threshold
+
+    spike_train, templates = gam_templates(
+        spike_train, path_to_recordings, CONFIG.spikeSize,
+        CONFIG.templatesMaxShift, merge_threshold, CONFIG.neighChannels)
+
+    Time['e'] += (datetime.datetime.now() - _b).total_seconds()
+
+    # report timing
+    currentTime = datetime.datetime.now()
+    logger.info("Templates done in {0} seconds.".format(
+        (currentTime - startTime).seconds))
+
+    return templates
