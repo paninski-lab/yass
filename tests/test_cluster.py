f7ddd916e0a46273feeace7ef8d7e60c8cbe30a2
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-19 20:33:25 -0500

    removes old test process, adds test cluster

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -10,7 +10,7 @@ import pytest
 import yass
 from yass import preprocess
 from yass import detect
-from yass import process
+from yass import cluster
 from yass import reset_config
 
 from util import clean_tmp
@@ -34,19 +34,17 @@ def test_process(path_to_config):
     (standarized_path, standarized_params, channel_index,
      whiten_filter) = preprocess.run()
 
-    (clear_scores,
-     spike_index_clear,
+    (score, spike_index_clear,
      spike_index_all) = detect.run(standarized_path,
                                    standarized_params,
                                    channel_index,
                                    whiten_filter)
 
-    (spike_train_clear,
-     templates) = process.run(clear_scores, spike_index_clear)
+    cluster.run(score, spike_index_clear)
 
     clean_tmp()
 
 
 def test_new_process_shows_error_if_empty_config():
     with pytest.raises(ValueError):
-        process.run(None, None, None)
+        cluster.run(None, None)
