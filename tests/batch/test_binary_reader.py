7b917b0a66d6279270786d2c587002d9eba72f53
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-21 15:28:51 -0500

    testnig binary reader

--- tests/batch/test_binary_reader.py
+++ tests/batch/test_binary_reader.py
@@ -33,8 +33,8 @@ def data_F(request):
     request.addfinalizer(delete_file)
 
     # column-major order
-    data = np.array(np.arange(1000)).reshape(10, 100).T
-    data.tofile(path)
+    data = np.array(np.arange(1000)).reshape(10, 100)
+    data.T.tofile(path)
 
     return data, path
 
@@ -49,3 +49,27 @@ def test_can_read_data_in_F_order(data_F):
     data, path = data_F
     f = BinaryReader(path, data.dtype, data.shape, 'F')
     np.testing.assert_equal(f[1:10, 2:3], data[1:10, 2:3])
+
+
+def test_can_read_data_in_C_order_empty_start(data_C):
+    data, path = data_C
+    c = BinaryReader(path, data.dtype, data.shape, 'C')
+    np.testing.assert_equal(c[:10, :3], data[:10, :3])
+
+
+def test_can_read_data_in_F_order_empty_start(data_F):
+    data, path = data_F
+    f = BinaryReader(path, data.dtype, data.shape, 'F')
+    np.testing.assert_equal(f[:10, :3], data[:10, :3])
+
+
+def test_can_read_data_in_C_order_empty_end(data_C):
+    data, path = data_C
+    c = BinaryReader(path, data.dtype, data.shape, 'C')
+    np.testing.assert_equal(c[:10, :3], data[:10, :3])
+
+
+def test_can_read_data_in_F_order_empty_end(data_F):
+    data, path = data_F
+    f = BinaryReader(path, data.dtype, data.shape, 'F')
+    np.testing.assert_equal(f[3:, 8:], data[3:, 8:])
