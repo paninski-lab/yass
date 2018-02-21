94e9df43980269b52da0a5bce4dc2b458dfd717f
hooshmandshr <shokri.hooshmand@gmail.com>
2018-02-21 11:43:46 -0500

    Fixing style issue.

--- src/yass/evaluate/stability.py
+++ src/yass/evaluate/stability.py
@@ -173,8 +173,8 @@ class RecordingAugmentation(object):
         self.n_chan = self.geometry.shape[0]
         self.template_calculator = mean_wave_calculator
         # Number of samples per batches.
-        self.batch_num_samples =\
-                self.template_calculator.batch_reader.batch_time_samples
+        n_samp = self.template_calculator.batch_reader.batch_time_samples
+        self.batch_num_samples = n_samp 
         self.construct_channel_map()
         self.compute_stat_summary()
         self.move_rate = move_rate
