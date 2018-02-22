168786e9f8229db0ba4bcffc72b3bd14447f3261
hooshmandshr <shokri.hooshmand@gmail.com>
2018-02-22 17:47:53 -0500

    Rafactored evaluation code uses config file.

---
+++ src/yass/evaluate/analyzer.py
@@ -0,0 +1,117 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import yaml
+
+from yass.evaluate.stability import (MeanWaveCalculator,
+                                     RecordingAugmentation,
+                                     RecordingBatchIterator,
+                                     SpikeSortingEvaluation)
+from yass.evaluate.visualization import ChristmassPlot
+from yass.pipeline import run
+
+
+class Analyzer(object):
+"""Class that analyzes the performance of yass on a certain dataset"""
+
+    def __init__(self, config, gold_std_spike_train):
+        """Sets up the analyzer object with configurations.
+
+        Parameters:
+            config: str
+            Absolute path to the yass config file for the dataset.
+            gold_std_spike_train: numpy.ndarray of shape (N, 2)
+            Gold standard spike train where first column corresponds to
+            spike times and second column corresponds to cluster identities.
+        """
+        self.config = config
+        self.gold_std_spike_train = gold_std_spike_train
+
+    def run(self):
+        """Runs the analyses on the dataset indicated in config."""
+
+        config_file = open(self.config, 'r')
+        config = yaml.load(config_file)
+        config_file.close()
+        # Extracting window around spikes.
+        sampling_rate = config['recordings']['sampling_rate']
+        n_chan = config['recordings']['n_channels']
+        dtype = config['recordings']['dtype']
+        spike_length = config['recordings']['spike_size_ms']
+        window_radius = int(spike_length * sampling_rate / 1e3)
+        window = range(-window_radius, window_radius)
+
+        # Set up the pyplot figures
+        # TODO(hooshmand): Set a name based on config file or user input.
+        plot_name = "Data"
+        stb_plot = ChristmasPlot(plot_name, k_tot_data, eval_type='Stability')
+        acc_plot = ChristmasPlot(plot_name, k_tot_data)
+
+        # Passing the config to pipeline to be run.
+        bin_file = config['data']['recordings']
+        geom_file = config['data']['geometry']
+        spike_train = run(config=config)
+        # Data augmentation setup.
+        os.path.getsize(bin_file)
+        file_size_bytes = os.path.getsize(bin_file)
+        tot_samples = file_size_bytes / (np.dtype(dtype).itemsize * n_chan)
+        radius = 70
+        n_batch_samples = int(tot_samples / n_batches)
+        batch_reader = RecordingBatchIterator(
+            bin_file, geom_file, sample_rate=sampling_rate,
+            batch_time_samples=n_batch_samples, n_batches=n_batches,
+            n_chan=n_chan, radius=radius, whiten=False)
+        mean_wave = MeanWaveCalculator(
+                batch_reader, spike_train, window=window)
+        mean_wave.compute_templates(n_batches=n_batches)
+        # Augment with new spikes.
+        stab = RecordingAugmentation(
+                mean_wave, augment_rate=0.25, move_rate=0.2)
+        aug_bin_file = '{}.aug'.format(bin_file)
+        aug_gold_spt, status = stab.save_augment_recording(
+                aug_bin_file, n_batches)
+        np.save('{}.aug.npy'.format(bin_file), aug_gold_spt)
+        # Setting up config file for yass to run on augmented data.
+        config['data']['recordings'] = aug_bin_file
+        config['data']['geometry'] = geom_file
+        yass_aug_spike_train = run(config=config)
+
+        # Evaluate accuracy of yass.
+        gold_std_spike_train_file = self.gold_std_spike_train
+        gold_standard_mean_wave = MeanWaveCalculator(
+            batch_reader, gold_std_spike_train, window=window)
+        gold_standard_mean_wave.compute_templates(n_batches=n_batches)
+        accuracy_eval = SpikeSortingEvaluation(
+            gold_std_spike_train, spike_train,
+            gold_standard_mean_wave.templates, mean_wave.templates)
+        acc_tp = accuracy_eval.true_positive
+        acc_plot.add_metric(
+                np.log(temp_snr(gold_standard_mean_wave.templates)),
+                acc_tp, data_idx)
+        batch_reader.close_iterator()
+
+        # Evaluate stability of yass.
+        batch_reader = RecordingBatchIterator(
+            aug_bin_file, geom_file, sample_rate=sampling_rate,
+            batch_time_samples=n_batch_samples, n_batches=n_batches,
+            n_chan=n_chan, radius=radius, whiten=False)
+        aug_gold_standard_mean_wave = MeanWaveCalculator(
+            batch_reader, aug_gold_spt, window=window)
+        aug_gold_standard_mean_wave.compute_templates(n_batches=n_batches)
+        aug_yass_mean_wave = MeanWaveCalculator(
+            batch_reader, yass_aug_spike_train, window=window)
+        aug_yass_mean_wave.compute_templates(n_batches=n_batches)
+        stability_eval = SpikeSortingEvaluation(
+            aug_gold_spt, yass_aug_spike_train,
+            aug_gold_standard_mean_wave.templates,
+            aug_yass_mean_wave.templates)
+        stb_tp = stability_eval.true_positive
+        stb_plot.add_metric(
+                np.log(temp_snr(aug_gold_standard_mean_wave.templates)),
+                stb_tp, data_idx)
+        batch_reader.close_iterator()
+
+        # Render the plots and save them.
+        acc_plot.generate_snr_metric_plot()
+        stb_plot.generate_snr_metric_plot()
+
---
+++ src/yass/evaluate/visualization.py
@@ -0,0 +1,129 @@
+"""Provides a set of standard plots for visualizing evaluations."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class ChristmasPlot(object):
+    """Standard figure for evaluation comparison vs. template properties."""
+
+    def __init__(self, data_set_title, n_dataset, methods=['Method'],
+                 logit_y=True, eval_type='Accuracy'):
+        """Setup pyplot figures.
+
+        Parameters
+        ----------
+        data_set_title: str
+            Title of the data set that will be displayed in the plots.
+        n_dataset: int
+            Total umber of data sets that evaluations are performed on.
+        methods: list of str
+            The spike sorting methods that evaluations are done for.
+        logit_y: bool
+            Logit transform the y-axis (metric axis) to emphasize near 1
+            and near 0 values.
+        eval_type: str
+            Type of metric (for display purposes only) which appears in the
+            plots.
+        """
+        self.new_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+                           '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+                           '#bcbd22', '#17becf']
+        self.n_dataset = n_dataset
+        self.methods = methods
+        self.data_set_title = data_set_title
+        self.eval_type = eval_type
+        self.logit_y = logit_y
+        self.data_set_title = data_set_title
+        # Contains evaluation metrics for methods and datasets.
+        self.metric_matrix = {}
+        for method in self.methods:
+            self.metric_matrix[method] = []
+            for i in range(self.n_dataset):
+                self.metric_matrix[method].append(None)
+
+    def logit(self, x, inverse=False):
+        """Logit transfors the array x.
+
+        Parameters
+        ----------
+        x: numpy.ndarray
+            List of values [0-1] only to be logit transformed.
+        inverse: bool
+            Inverse-logit transforms if True.
+        """
+        # Add apsilon to avoid boundary conditions for transform.
+        x[x == 0] += 0.0001
+        x[x == 1] -= 0.0001
+        if inverse:
+            return 1 / (1 + np.exp(-x))
+        return np.log(x / (1 - x))
+
+    def set_logit_labels(
+            self, labs=np.array([0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999])):
+        """Logit transforms the y axis.
+
+        Parameters
+        ----------
+        labs: numpy.ndarray
+            List of values ([0-1] only) to be displayed as ticks on
+            y axis.
+        """
+        for i in range(self.n_dataset):
+            self.ax[i].set_yticks(self.logit(labs))
+            self.ax[i].set_yticklabels(labs)
+
+    def add_metric(self, snr_list, percent_list, dataset_number,
+                   method_name='Method'):
+        """Adds accuracy percentages for clusters/units of a method.
+
+        Parameters
+        ----------
+        snr_list: numpy.ndarray of shape(N,)
+            List of SNR/PNR values for clusters/units of the corresponding
+            dataset number and spike sorting method.
+        percent_list: numpy.ndarray of shape(N,)
+            List of SNR/PNR values for clusters/units of the corresponding
+            dataset number and spike sorting method.
+        dataset_number: int
+            Value should be between 0 and self.n_dataset - 1. Indicates
+            which dataset are the evaluations for.
+        method: str
+            Should be a member of self.methods. Indicates which of the
+            spike sorting methods the evaluations correspond to.
+        """
+        if method_name not in self.methods:
+            raise KeyError('Method name does not exist in methods list.')
+        if np.any(percent_list < 0) or np.any(percent_list > 1):
+            raise TypeError(
+                    'Percent accuracy list should contain only [0-1] values.')
+        eval_tup = (snr_list, percent_list)
+        self.metric_matrix[method_name][dataset_number] = eval_tup
+
+    def generate_snr_metric_plot(self):
+        """Generate pdf plots of evaluations for the datasets and methods."""
+        self.fig, self.ax = plt.subplots(self.n_dataset, 1)
+        for i in range(self.n_dataset):
+            self.ax[i].set_title(
+                    self.data_set_title + 'Dataset {}'.format(i + 1))
+            self.ax[i].set_ylabel('Percent {}'.format(self.eval_type))
+            self.ax[i].legend(self.methods)
+        self.ax[i].set_xlabel('Log PNR')
+        if self.logit_y:
+            self.set_logit_labels()
+        for method_idx, method in enumerate(self.methods):
+            for i in range(self.n_dataset):
+                try:
+                    metric_tuple = self.metric_matrix[method][i]
+                    metrics = metric_tuple[1]
+                    if self.logit_y:
+                        metrics = self.logit(metrics)
+                    self.ax[i].scatter(
+                        metric_tuple[0], metrics,
+                        color=self.new_colors[method_idx])
+                except Exception:
+                    print("No metric found for {} for dataset {}".format(
+                        method, i + 1))
+        self.fig.set_size_inches(12, 4 * self.n_dataset)
+        plt.savefig('{}_{}.pdf'.format(self.data_set_title, self.eval_type))
+
