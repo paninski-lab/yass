c70eb11cccc06747e51e8c1bc0412489653026e1
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-03-20 00:08:19 -0400

    verifying all sample config files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-
 import pytest
 
 
@@ -10,20 +9,34 @@ def path_to_tests():
 
 
 @pytest.fixture(scope='session')
+def path_to_examples():
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        '../examples')
+    return path
+
+
+@pytest.fixture(scope='session')
 def path_to_data_folder():
     path = os.path.dirname(os.path.realpath(__file__))
     return os.path.join(path, 'data/')
 
 
 @pytest.fixture
-def path_to_nnet_config():
+def path_to_nnet_config(scope='session'):
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                         'config_nnet.yaml')
     return path
 
 
 @pytest.fixture
-def path_to_threshold_config():
+def path_to_threshold_config(scope='session'):
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                         'config_threshold.yaml')
+    return path
+
+
+@pytest.fixture
+def path_to_config_sample(scope='session'):
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                        'config_sample.yaml')
     return path
