a14cf7612bd34837c67653a6e1b99a299b62f55d
Peter Lee <jinhyungp.lee@gmail.com>
2018-03-26 14:23:16 -0400

    fix tests

--- examples/pipeline/deconvolute.py
+++ examples/pipeline/deconvolute.py
@@ -23,7 +23,8 @@ yass.set_config('config_sample.yaml')
                                whiten_filter)
 
 
-spike_train_clear = cluster.run(score, spike_index_clear)
+spike_train_clear, score_clear = cluster.run(
+    score, spike_index_clear)
 
 templates = templates.run(spike_train_clear)
 
