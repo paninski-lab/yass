192540e71dd04460cd3ecf70f8b0114081b370b9
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-05-02 10:26:14 -0400

    testing neural network output

--- tests/test_neuralnet.py
+++ tests/test_neuralnet.py
@@ -11,14 +11,16 @@ from yass.batch import RecordingsReader, BatchProcessor
 from yass import neuralnetwork
 from yass.geometry import make_channel_index, n_steps_neigh_channels
 
+SAVE = False
 
-def test_can_use_neural_network_detector(path_to_tests):
+
+def run_nnet(path_to_tests):
     yass.set_config(path.join(path_to_tests, 'config_nnet.yaml'))
     CONFIG = yass.read_config()
 
     data = RecordingsReader(path.join(path_to_tests,
                                       'data/standarized.bin'),
-                            loader='array').data.T
+                            loader='array').data
 
     channel_index = make_channel_index(CONFIG.neigh_channels,
                                        CONFIG.geom)
@@ -43,8 +45,39 @@ def test_can_use_neural_network_detector(path_to_tests):
                                            )
 
     neighbors = n_steps_neigh_channels(CONFIG.neigh_channels, 2)
-    neuralnetwork.run_detect_triage_featurize(data, x_tf, output_tf,
-                                              NND, NNAE, NNT, neighbors)
+    return neuralnetwork.run_detect_triage_featurize(data, x_tf, output_tf,
+                                                     NND, NNAE, NNT, neighbors)
+
+
+def test_can_use_neural_network_detector(path_to_tests):
+    scores, clear, collision = run_nnet(path_to_tests)
+
+
+def test_same_result_as_before(path_to_tests, path_to_data_folder):
+    scores, clear, collision = run_nnet(path_to_tests)
+
+    path_to_clear = path.join(path_to_data_folder,
+                              'output_reference',
+                              'nnet_detector_clear.npy')
+    path_to_scores = path.join(path_to_data_folder,
+                               'output_reference',
+                               'nnet_detector_scores.npy')
+    path_to_collision = path.join(path_to_data_folder,
+                                  'output_reference',
+                                  'nnet_detector_collision.npy')
+
+    if SAVE:
+        np.save(path_to_clear, clear)
+        np.save(path_to_scores, scores)
+        np.save(path_to_collision, collision)
+
+    clear_saved = np.load(path_to_clear)
+    scores_saved = np.load(path_to_scores)
+    collision_saved = np.load(path_to_collision)
+
+    np.testing.assert_array_equal(clear_saved, clear)
+    np.testing.assert_array_equal(scores_saved, scores)
+    np.testing.assert_array_equal(collision_saved, collision)
 
 
 def test_splitting_in_batches_does_not_affect_result(path_to_tests):
@@ -79,14 +112,22 @@ def test_splitting_in_batches_does_not_affect_result(path_to_tests):
                                            triage_fname,
                                            )
 
-    # buffer size makes sure we can detect spikes if they appear at the end of
-    # any batch
+    neighbors = n_steps_neigh_channels(CONFIG.neigh_channels, 2)
+
+    # run all at once
+    (scores, clear,
+     collision) = neuralnetwork.run_detect_triage_featurize(data, x_tf,
+                                                            output_tf,
+                                                            NND, NNAE,
+                                                            NNT, neighbors)
+
+    # run in batches - buffer size makes sure we can detect spikes if they
+    # appear at the end of any batch
     bp = BatchProcessor(PATH_TO_DATA, PARAMS['dtype'], PARAMS['n_channels'],
                         PARAMS['data_order'], '100KB',
                         buffer_size=CONFIG.spike_size)
-    mc = bp.multi_channel_apply
-    neighbors = n_steps_neigh_channels(CONFIG.neigh_channels, 2)
-    res = mc(
+
+    res = bp.multi_channel_apply(
         neuralnetwork.run_detect_triage_featurize,
         mode='memory',
         cleanup_function=neuralnetwork.fix_indexes,
@@ -101,16 +142,6 @@ def test_splitting_in_batches_does_not_affect_result(path_to_tests):
     clear_batch = np.concatenate([element[1] for element in res], axis=0)
     collision_batch = np.concatenate([element[2] for element in res], axis=0)
 
-    (scores, clear,
-     collision) = neuralnetwork.run_detect_triage_featurize(data, x_tf,
-                                                            output_tf,
-                                                            NND, NNAE,
-                                                            NNT, neighbors)
-
     np.testing.assert_array_equal(clear_batch, clear)
     np.testing.assert_array_equal(collision_batch, collision)
     np.testing.assert_array_equal(scores_batch, scores)
-
-
-def test_can_train_nnet(path_to_tests):
-    pass
