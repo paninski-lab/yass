fe4d431006630a5525d13c18c780f10f3080f5a6
Eduardo Blancas <fkq8@blancas.io>
2018-05-25 15:23:35 -0400

    pca test

--- tests/test_threshold.py
+++ tests/test_threshold.py
@@ -1,10 +1,9 @@
-# from yass.threshold.dimensionality_reduction import pca
+from yass.threshold.dimensionality_reduction import pca
 
 
 def test_can_compute_pca(path_to_data, data_info):
-    pass
-    # pca(path_to_data, data_info['dtype'], data_info['n_channels'],
-    # data_info['data_order'], data_info['recordings'])
+    pca(path_to_data, data_info['dtype'], data_info['n_channels'],
+        data_info['data_order'], max_memory='1GB')
 
 
 def test_pca_is_not_run_if_files_already_exist():
