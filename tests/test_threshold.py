149d5d3a96ba3084a4580042d8b366ce5d4343f7
Eduardo Blancas <fkq8@blancas.io>
2018-05-25 14:58:30 -0400

    removes unused parameter in pca function

---
+++ tests/test_threshold.py
@@ -0,0 +1,15 @@
+# from yass.threshold.dimensionality_reduction import pca
+
+
+def test_can_compute_pca(path_to_data, data_info):
+    pass
+    # pca(path_to_data, data_info['dtype'], data_info['n_channels'],
+    # data_info['data_order'], data_info['recordings'])
+
+
+def test_pca_is_not_run_if_files_already_exist():
+    pass
+
+
+def test_threshold_detector_is_not_run_if_files_already_exist():
+    pass
