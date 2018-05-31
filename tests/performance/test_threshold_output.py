0142be9caee2cc07501eb4d41ce82246bad1824b
Eduardo Blancas <fkq8@blancas.io>
2018-05-31 13:06:53 -0400

    updates testing file

--- tests/performance/test_threshold_output.py
+++ tests/performance/test_threshold_output.py
@@ -1,12 +1,14 @@
 from os import path
 import numpy as np
 import yass
-from yass import preprocess, detect, cluster
+from yass import preprocess, detect, cluster, templates, deconvolute
 
 PATHT_TO_REF = '/Users/Edu/data/yass/ref49/'
 
 
 def test_threshold_output(path_to_tests):
+    """Test that pipeline using threshold detector returns the same results
+    """
     yass.set_config(path.join(path_to_tests, 'config_threshold_49.yaml'))
 
     (standarized_path, standarized_params, channel_index,
@@ -24,6 +26,7 @@ def test_threshold_output(path_to_tests):
     # test preprocess output
     np.testing.assert_array_equal(channel_index_saved, channel_index)
     np.testing.assert_array_equal(whitening_saved, whiten_filter)
+    # TODO: check standarized data
 
     (score, spike_index_clear,
      spike_index_all) = detect.run(standarized_path,
@@ -58,5 +61,19 @@ def test_threshold_output(path_to_tests):
     np.testing.assert_array_equal(spike_train_cluster_saved, spike_train_clear)
 
     # test templates
+    (templates_, spike_train,
+     groups, idx_good_templates) = templates.run(spike_train_clear, tmp_loc,
+                                                 save_results=True)
+
+    path_to_templates = path.join(PATHT_TO_REF, 'templates', 'templates.npy')
+    templates_saved = np.load(path_to_templates)
+
+    np.testing.assert_array_equal(templates_saved, templates_)
 
     # test deconvolution
+    spike_train = deconvolute.run(spike_index_all, templates_)
+
+    path_to_spike_train = path.join(PATHT_TO_REF, 'spike_train.npy')
+    spike_train_saved = np.load(path_to_spike_train)
+
+    np.testing.assert_array_equal(spike_train_saved, spike_train)
