f182e40a6a41c0e2d791940cd3a413e3a485208f
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-06-21 19:09:46 -0400

    removes old import

--- src/yass/neuralnetwork/__init__.py
+++ src/yass/neuralnetwork/__init__.py
@@ -1,8 +1,7 @@
 from yass.neuralnetwork.nndetector import NeuralNetDetector
 from yass.neuralnetwork.nnae import AutoEncoder
 from yass.neuralnetwork.nntriage import NeuralNetTriage
-from yass.neuralnetwork.prepare import prepare_nn
 from yass.neuralnetwork.apply import run_detect_triage_featurize, fix_indexes
 
-__all__ = ['NeuralNetDetector', 'NeuralNetTriage', 'prepare_nn',
+__all__ = ['NeuralNetDetector', 'NeuralNetTriage',
            'run_detect_triage_featurize', 'fix_indexes', 'AutoEncoder']
