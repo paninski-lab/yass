b9d186793b0d89fc9db1cc77124f85d2131207f4
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-06-21 19:21:21 -0400

    renaming

--- src/yass/neuralnetwork/__init__.py
+++ src/yass/neuralnetwork/__init__.py
@@ -1,6 +1,6 @@
-from yass.neuralnetwork.nndetector import NeuralNetDetector
-from yass.neuralnetwork.nnae import AutoEncoder
-from yass.neuralnetwork.nntriage import NeuralNetTriage
+from yass.neuralnetwork.model_detector import NeuralNetDetector
+from yass.neuralnetwork.model_autoencoder import AutoEncoder
+from yass.neuralnetwork.model_triage import NeuralNetTriage
 from yass.neuralnetwork.apply import run_detect_triage_featurize, fix_indexes
 
 __all__ = ['NeuralNetDetector', 'NeuralNetTriage',
