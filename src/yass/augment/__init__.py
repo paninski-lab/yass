2a99a9c82ad039748912836ec5c2337681de0b43
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-06-21 21:54:14 -0400

    removes old imports

--- src/yass/augment/__init__.py
+++ src/yass/augment/__init__.py
@@ -1,6 +1,3 @@
-from yass.augment.train_detector import train_detector
-from yass.augment.train_ae import train_ae
-from yass.augment.train_triage import train_triage
 from yass.augment.train_all import train_neural_networks
 from yass.augment.make import make_training_data
 from yass.augment.util import (save_detect_network_params,
@@ -8,7 +5,6 @@ from yass.augment.util import (save_detect_network_params,
                                save_ae_network_params)
 
 __all__ = [
-    'train_detector', 'train_ae', 'train_triage',
     'train_neural_networks', 'make_training_data',
     'save_detect_network_params', 'save_triage_network_params',
     'save_ae_network_params'
