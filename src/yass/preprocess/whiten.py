7abec0f7d61d03035a57899cfc13dbb6f66152f5
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-06-26 10:49:14 -0400

    fixes #220

--- src/yass/preprocess/whiten.py
+++ src/yass/preprocess/whiten.py
@@ -148,7 +148,7 @@ def _matrix(recording, channel_index, spike_size):
     M = np.matmul(blanked_rec.transpose(), blanked_rec) / \
         np.matmul(spikes_rec.transpose(), spikes_rec)
 
-    # since recording is standardized recording, covaraince = correlation
+    # since recording is standarized recording, covaraince = correlation
     invhalf_var = np.diag(np.power(np.diag(M), -0.5))
     M = np.matmul(np.matmul(invhalf_var, M), invhalf_var)
 
