37888618b653db6a0c03fea62cfbc8724491a64f
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-06-26 21:04:32 -0400

    triage refactor

--- src/yass/neuralnetwork/model_triage.py
+++ src/yass/neuralnetwork/model_triage.py
@@ -45,25 +45,65 @@ class NeuralNetTriage(object):
     """
 
     def __init__(self, path_to_model, threshold,
-                 input_tensor=None, parameters=None):
+                 input_tensor=None, params=None, n_batch=None,
+                 l2_reg_scale=None, train_step_size=None, n_iter=None):
+        self.path_to_model = path_to_model
+        self.threshold = threshold
+        self.params = params
+        self.n_batch = n_batch
+        self.l2_reg_scale = l2_reg_scale
+        self.train_step_size = train_step_size
+        self.n_iter = n_iter
 
-        # restoring a model
-        if path_to_model:
-            if not path_to_model.endswith('.ckpt'):
-                path_to_model = path_to_model+'.ckpt'
+        self.idx_clean = self._make_graph(threshold, input_tensor,
+                                          self.params['filters'],
+                                          self.params['size'],
+                                          self.params['n_neigh'])
 
-            self.path_to_model = path_to_model
+    @classmethod
+    def load_from_file(cls, path_to_model, threshold, input_tensor=None):
+        """Load a model from a file
+        """
+        if not path_to_model.endswith('.ckpt'):
+            path_to_model = path_to_model+'.ckpt'
 
-            # load necessary parameters
-            path_to_filters = change_extension(path_to_model, 'yaml')
-            params = load_yaml(path_to_filters)
+        # load necessary parameters
+        path_to_filters = change_extension(path_to_model, 'yaml')
+        params = load_yaml(path_to_filters)
 
-            size = params['size']
-            filters = params['filters']
-            n_neigh = params['n_neighbors']
+        return NeuralNetTriage(path_to_model, threshold, input_tensor,
+                               params)
 
-            self.idx_clean = self._make_graph(threshold, input_tensor,
-                                              filters, size, n_neigh)
+    @classmethod
+    def _make_network(cls, input_tensor, filters, size, n_neigh):
+        """Mates tensorflow network, from first layer to output layer
+        """
+        K1, K2 = filters
+
+        # initialize and save nn weights
+        W1 = weight_variable([size, 1, 1, K1])
+        b1 = bias_variable([K1])
+
+        W11 = weight_variable([1, 1, K1, K2])
+        b11 = bias_variable([K2])
+
+        W2 = weight_variable([1, n_neigh, K2, 1])
+        b2 = bias_variable([1])
+
+        # first layer: temporal feature
+        layer1 = tf.nn.relu(conv2d_VALID(tf.expand_dims(input_tensor, -1),
+                                         W1) + b1)
+
+        # second layer: feataure mapping
+        layer11 = tf.nn.relu(conv2d(layer1, W11) + b11)
+
+        # third layer: spatial convolution
+        o_layer = conv2d_VALID(layer11, W2) + b2
+
+        vars_dict = {"W1": W1, "W11": W11, "W2": W2, "b1": b1, "b11": b11,
+                     "b2": b2}
+
+        return o_layer, vars_dict
 
     def _make_graph(self, threshold, input_tensor, filters, size, n_neigh):
         """Builds graph for triage
@@ -98,37 +138,6 @@ class NeuralNetTriage(object):
         # thrshold it
         return self.o_layer[:, 0, 0, 0] > np.log(threshold / (1 - threshold))
 
-    @classmethod
-    def _make_network(cls, input_tensor, filters, size, n_neigh):
-        """Mates tensorflow network, from first layer to output layer
-        """
-        K1, K2 = filters
-
-        # initialize and save nn weights
-        W1 = weight_variable([size, 1, 1, K1])
-        b1 = bias_variable([K1])
-
-        W11 = weight_variable([1, 1, K1, K2])
-        b11 = bias_variable([K2])
-
-        W2 = weight_variable([1, n_neigh, K2, 1])
-        b2 = bias_variable([1])
-
-        # first layer: temporal feature
-        layer1 = tf.nn.relu(conv2d_VALID(tf.expand_dims(input_tensor, -1),
-                                         W1) + b1)
-
-        # second layer: feataure mapping
-        layer11 = tf.nn.relu(conv2d(layer1, W11) + b11)
-
-        # third layer: spatial convolution
-        o_layer = conv2d_VALID(layer11, W2) + b2
-
-        vars_dict = {"W1": W1, "W11": W11, "W2": W2, "b1": b1, "b11": b11,
-                     "b2": b2}
-
-        return o_layer, vars_dict
-
     def restore(self, sess):
         """Restore tensor values
         """
@@ -145,11 +154,8 @@ class NeuralNetTriage(object):
 
         return idx_clean
 
-    @classmethod
-    def train(cls, x_train, y_train, filters, n_iter, n_batch,
-              l2_reg_scale, train_step_size, path_to_model):
-        """
-        Trains the triage network
+    def fit(self, x_train, y_train):
+        """Trains the triage network
 
         Parameters:
         -----------
@@ -163,16 +169,13 @@ class NeuralNetTriage(object):
         """
         logger = logging.getLogger(__name__)
 
-        if not path_to_model.endswith('.ckpt'):
-            path_to_model = path_to_model+'.ckpt'
-
         # get parameters
         n_data, size, n_neigh = x_train.shape
-        K1, K2 = filters
+        filters = self.params['filters']
 
         # x and y input tensors
-        x_tf = tf.placeholder("float", [n_batch, size, n_neigh])
-        y_tf = tf.placeholder("float", [n_batch])
+        x_tf = tf.placeholder("float", [self.n_batch, size, n_neigh])
+        y_tf = tf.placeholder("float", [self.n_batch])
 
         o_layer, vars_dict = NeuralNetTriage._make_network(x_tf, filters,
                                                            size, n_neigh)
@@ -185,13 +188,14 @@ class NeuralNetTriage(object):
 
         # regularization term
         weights = tf.trainable_variables()
-        l2_regularizer = tf.contrib.layers.l2_regularizer(scale=l2_reg_scale)
+        l2_regularizer = (tf.contrib.layers
+                            .l2_regularizer(scale=self.l2_reg_scale))
         regularization_penalty = tf.contrib.layers.apply_regularization(
             l2_regularizer, weights)
         regularized_loss = cross_entropy + regularization_penalty
 
         # train step
-        train_step = tf.train.AdamOptimizer(train_step_size).minimize(
+        train_step = tf.train.AdamOptimizer(self.train_step_size).minimize(
             regularized_loss)
 
         # saver
@@ -203,15 +207,16 @@ class NeuralNetTriage(object):
 
         logger.info('Training triage network...')
 
-        bar = tqdm(total=n_iter)
+        bar = tqdm(total=self.n_iter)
 
         with tf.Session() as sess:
             init_op = tf.global_variables_initializer()
             sess.run(init_op)
 
-            for i in range(0, n_iter):
+            for i in range(0, self.n_iter):
 
-                idx_batch = np.random.choice(n_data, n_batch, replace=False)
+                idx_batch = np.random.choice(n_data, self.n_batch,
+                                             replace=False)
                 sess.run(
                     train_step,
                     feed_dict={
@@ -220,9 +225,9 @@ class NeuralNetTriage(object):
                     })
                 bar.update(i + 1)
 
-            saver.save(sess, path_to_model)
+            saver.save(sess, self.path_to_model)
 
-            idx_batch = np.random.choice(n_data, n_batch, replace=False)
+            idx_batch = np.random.choice(n_data, self.n_batch, replace=False)
             output = sess.run(o_layer, feed_dict={x_tf: x_train[idx_batch]})
             y_test = y_train[idx_batch]
             tp = np.mean(output[y_test == 1] > 0)
@@ -233,8 +238,8 @@ class NeuralNetTriage(object):
         bar.close()
 
         logger.info('Saving triage network parameters...')
+        path_to_params = change_extension(self.path_to_model, 'yaml')
         save_triage_network_params(filters=filters,
                                    size=x_train.shape[1],
                                    n_neighbors=x_train.shape[2],
-                                   output_path=change_extension(path_to_model,
-                                                                'yaml'))
+                                   output_path=path_to_params)
