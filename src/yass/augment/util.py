7160336717d7ad684e16c3b0903f8fe39f974d21
Eduardo Blancas <fkq8@blancas.io>
2018-07-11 13:51:54 -0500

    fix

--- src/yass/augment/util.py
+++ src/yass/augment/util.py
@@ -134,7 +134,10 @@ def make_collided(x_clean, collision_ratio, templates, max_shift,
         else:
             to_add[j] += x_to_collide
 
-    return x_collision if not DEBUG_MODE else x_collision, x_to_collide_all
+    if DEBUG_MODE:
+        return x_collision, x_to_collide_all
+    else:
+        return x_collision
 
 
 def make_misaligned(x_clean, templates, max_shift, misalign_ratio,
