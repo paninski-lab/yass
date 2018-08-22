5362c21e6c1bb1782a623eda8b0cda53917bb7ae
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-08-22 11:56:26 -0400

    deletes neuralnetwork/train.py

--- src/yass/neuralnetwork/__init__.py
+++ src/yass/neuralnetwork/__init__.py
@@ -3,8 +3,7 @@ from yass.neuralnetwork.model_detector import NeuralNetDetector
 from yass.neuralnetwork.model_autoencoder import AutoEncoder
 from yass.neuralnetwork.model_triage import NeuralNetTriage
 from yass.neuralnetwork.apply import run_detect_triage_featurize, fix_indexes
-from yass.neuralnetwork.train import train
 
 __all__ = ['NeuralNetDetector', 'NeuralNetTriage',
            'run_detect_triage_featurize', 'fix_indexes', 'AutoEncoder',
-           'train', 'KerasModel']
+           'KerasModel']
