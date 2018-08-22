f2ff20654e63707991853881d326cca78bbaae7c
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-08-22 14:52:11 -0400

    updated filtering functions from master

--- src/yass/preprocess/filter.py
+++ src/yass/preprocess/filter.py
@@ -1,38 +1,27 @@
 """
 Filtering functions
 """
+from functools import partial
 import os
 import numpy as np
 import os.path
 
+import multiprocess
 from scipy.signal import butter, lfilter, filtfilt
 
 from yass.batch import BatchProcessor
 from yass.util import check_for_files, ExpandPath, LoadFile
-from yass.preprocess.standarize import _standard_deviation
+from yass.preprocess.standarize import _standard_deviation, standard_deviation
 
 
-# *** DEPRECATED ****
-@check_for_files(
-    filenames=[
-        ExpandPath('output_filename'),
-        LoadFile('output_filename', 'yaml')
-    ],
-    mode='extract',
-    relative_to='output_path')
-def butterworth(path_to_data,
-                dtype,
-                n_channels,
-                data_order,
-                low_frequency,
-                high_factor,
-                order,
-                sampling_frequency,
-                max_memory,
-                output_path,
-                output_dtype,
-                output_filename='filtered.bin',
-                if_file_exists='skip'):
+@check_for_files(filenames=[ExpandPath('output_filename'),
+                            LoadFile('output_filename', 'yaml')],
+                 mode='extract', relative_to='output_path')
+def butterworth(path_to_data, dtype, n_channels, data_order,
+                low_frequency, high_factor, order, sampling_frequency,
+                max_memory, output_path, output_dtype, standarize=False,
+                output_filename='filtered.bin', if_file_exists='skip',
+                processes='max'):
     """Filter (butterworth) recordings in batches
 
     Parameters
@@ -78,11 +67,18 @@ def butterworth(path_to_data,
     output_dtype: str
         dtype for filtered data
 
+    standarize: bool
+        Whether to standarize the data after the filtering step
+
     if_file_exists: str, optional
         One of 'overwrite', 'abort', 'skip'. If 'overwrite' it replaces the
         file if it exists, if 'abort' if raise a ValueError exception if
         the file exists, if 'skip' if skips the operation if the file
         exists
+
+    processes: str or int, optional
+        Number of processes to use, if 'max', it uses all cores in the machine
+        if a number, it uses that number of cores
 
     Returns
     -------
@@ -93,35 +89,62 @@ def butterworth(path_to_data,
         A dictionary with the parameters for the filtered recordings
         (dtype, n_channels, data_order)
     """
+    processes = multiprocess.cpu_count() if processes == 'max' else processes
+
     # init batch processor
-    bp = BatchProcessor(
-        path_to_data,
-        dtype,
-        n_channels,
-        data_order,
-        max_memory,
-        buffer_size=200)
+    bp = BatchProcessor(path_to_data, dtype, n_channels, data_order,
+                        max_memory, buffer_size=200)
+
+    if standarize:
+        bp_ = BatchProcessor(path_to_data, dtype, n_channels, data_order,
+                             max_memory, buffer_size=0)
+
+        filtering = partial(_butterworth, low_frequency=low_frequency,
+                            high_factor=high_factor,
+                            order=order,
+                            sampling_frequency=sampling_frequency)
+
+        # if standarize, estimate sd from first batch and use
+        # _butterworth_scale function, pass filtering to estimate sd from the
+        # filtered data
+        sd = standard_deviation(bp_, sampling_frequency,
+                                preprocess_fn=filtering)
+        fn = partial(_butterworth_scale, denominator=sd)
+        # add name to the partial object, since it is not added...
+        fn.__name__ = _butterworth_scale.__name__
+    else:
+        # otherwise use _butterworth function
+        fn = _butterworth
 
     _output_path = os.path.join(output_path, output_filename)
 
-    (path, params) = bp.multi_channel_apply(
-        _butterworth,
-        mode='disk',
-        cleanup_function=fix_indexes,
-        output_path=_output_path,
-        if_file_exists=if_file_exists,
-        cast_dtype=output_dtype,
-        low_frequency=low_frequency,
-        high_factor=high_factor,
-        order=order,
-        sampling_frequency=sampling_frequency)
+    (path,
+     params) = bp.multi_channel_apply(fn, mode='disk',
+                                      cleanup_function=fix_indexes,
+                                      output_path=_output_path,
+                                      if_file_exists=if_file_exists,
+                                      cast_dtype=output_dtype,
+                                      low_frequency=low_frequency,
+                                      high_factor=high_factor,
+                                      order=order,
+                                      sampling_frequency=sampling_frequency,
+                                      processes=processes)
 
     return path, params
 
 
-# **** DEPRECATED *****
+def _butterworth_scale(ts, low_frequency, high_factor, order,
+                       sampling_frequency, denominator):
+    """Filter and then divide
+    """
+    filtered = _butterworth(ts, low_frequency, high_factor, order,
+                            sampling_frequency)
+
+    return np.divide(filtered, denominator)
+
+
 def _butterworth(ts, low_frequency, high_factor, order, sampling_frequency):
-    """Butterworth filter for a one dimensional time series
+    """Butterworth filter
 
     Parameters
     ----------
@@ -149,24 +172,21 @@ def _butterworth(ts, low_frequency, high_factor, order, sampling_frequency):
 
     if ts.ndim == 1:
 
-        (T, ) = ts.shape
-        low = float(low_frequency) / sampling_frequency * 2
-        high = float(high_factor) * 2
-        b, a = butter(order, [low, high], btype='band')
+        (T,) = ts.shape
+        low = float(low_frequency)/sampling_frequency * 2
+        b, a = butter(order, low, btype='highpass')
 
-        return lfilter(b, a, ts)
+        return filtfilt(b, a, ts)
 
     else:
 
         T, C = ts.shape
-        low = float(low_frequency) / sampling_frequency * 2
-        high = float(high_factor) * 2
-        b, a = butter(order, [low, high], btype='band')
+        low = float(low_frequency)/sampling_frequency * 2
+        b, a = butter(order, low, btype='highpass')
 
-        return lfilter(b, a, ts, 0)
+        return filtfilt(b, a, ts, 0)
 
 
-# **** DEPRECATED *****
 def fix_indexes(res, idx_local, idx, buffer_size):
     """Fixes indexes from detected spikes in batches
 
@@ -187,6 +207,9 @@ def fix_indexes(res, idx_local, idx, buffer_size):
     data_end = idx_local[0].stop
 
     return res[data_start:data_end]
+
+
+# Cat's code below this point
 
 
 def filter_standardize_parallel(data_in, low_frequency, high_factor, order,
