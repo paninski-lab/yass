f63f60da6480cc3abe1ca50eff72885d277b8905
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-09-05 13:49:48 -0400

    more merging from master

--- src/yass/detect/__init__.py
+++ src/yass/detect/__init__.py
@@ -2,7 +2,6 @@
 Detecting spikes
 """
 
-from yass.detect.run2 import run2
 from yass.detect.run import run
 
 __all__ = ['run']
--- src/yass/empty.py
+++
@@ -1,9 +0,0 @@
-class empty:
-
-    def __init__(self):                
-        pass
-        #self.recordings = lambda:None
-        #self.resources = lambda:None
-
-class attribs: 
-    pass
