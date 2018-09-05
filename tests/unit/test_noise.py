8084c4778c046951c0c519d8a7f50317b9ed5b5a
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-09-05 14:07:54 -0400

    adds test noise from master

---
+++ tests/unit/test_noise.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from yass.augment import noise
+from yass.batch import RecordingsReader
+
+
+def test_can_kill_signal(path_to_standarized_data, path_to_threshold_config):
+    recordings = RecordingsReader(path_to_standarized_data,
+                                  loader='array')._data
+
+    noise.kill_signal(recordings,
+                      threshold=3.0,
+                      window_size=10)
+
+
+def test_can_estimate_temporal_and_spatial_sig(path_to_standarized_data,
+                                               path_to_threshold_config):
+
+    recordings = RecordingsReader(path_to_standarized_data,
+                                  loader='array')._data
+
+    (spatial_SIG,
+        temporal_SIG) = noise.noise_cov(recordings,
+                                        temporal_size=40,
+                                        sample_size=1000,
+                                        threshold=3.0,
+                                        window_size=10)
+
+    # check no nans
+    assert (~np.isnan(spatial_SIG)).all()
+    assert (~np.isnan(temporal_SIG)).all()
