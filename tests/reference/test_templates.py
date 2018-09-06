52de816ac719d52047c4fb72e8c88ff1f5edd07f
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-09-06 13:30:11 -0400

    moves test templates reference

---
+++ tests/reference/test_templates.py
@@ -0,0 +1,39 @@
+from os import path
+
+import yass
+from yass import preprocess
+from yass import detect
+from yass import cluster
+from yass import templates
+
+from util import ReferenceTesting
+
+
+def test_templates_returns_expected_results(path_to_threshold_config,
+                                            path_to_output_reference,
+                                            make_tmp_folder):
+
+    yass.set_config(path_to_threshold_config, make_tmp_folder)
+
+    (standarized_path,
+     standarized_params,
+     whiten_filter) = preprocess.run()
+
+    (spike_index_clear,
+     spike_index_all) = detect.run(standarized_path,
+                                   standarized_params,
+                                   whiten_filter)
+
+    (spike_train_clear,
+     tmp_loc,
+     vbParam) = cluster.run(spike_index_clear)
+
+    (templates_, spike_train,
+     groups,
+     idx_good_templates) = templates.run(spike_train_clear, tmp_loc,
+                                         save_results=True)
+
+    path_to_templates = path.join(path_to_output_reference,
+                                  'templates.npy')
+
+    ReferenceTesting.assert_array_equal(templates_, path_to_templates)
--- tests/unit/test_templates.py
+++ tests/unit/test_templates.py
@@ -2,17 +2,11 @@
 process.run tests, checking that the pipeline finishes without errors for
 several configuration files
 """
-from os import path
-
-import pytest
-
 import yass
 from yass import preprocess
 from yass import detect
 from yass import cluster
 from yass import templates
-
-from util import ReferenceTesting
 
 
 def test_templates(path_to_threshold_config, make_tmp_folder):
@@ -79,37 +73,5 @@ def test_templates_loads_from_disk_if_files_exist(make_tmp_folder,
     templates.run(spike_train_clear, tmp_loc, save_results=True)
 
     assert not templates.run.executed
-
-
-@pytest.mark.xfail
-def test_templates_returns_expected_results(path_to_threshold_config,
-                                            path_to_output_reference,
-                                            make_tmp_folder):
-
-    yass.set_config(path_to_threshold_config, make_tmp_folder)
-
-    (standarized_path,
-     standarized_params,
-     whiten_filter) = preprocess.run()
-
-    (spike_index_clear,
-     spike_index_all) = detect.run(standarized_path,
-                                   standarized_params,
-                                   whiten_filter)
-
-    (spike_train_clear,
-     tmp_loc,
-     vbParam) = cluster.run(spike_index_clear)
-
-    (templates_, spike_train,
-     groups,
-     idx_good_templates) = templates.run(spike_train_clear, tmp_loc,
-                                         save_results=True)
-
-    path_to_templates = path.join(path_to_output_reference,
-                                  'templates.npy')
-
-    ReferenceTesting.assert_array_equal(templates_, path_to_templates)
-
 
 # TODO: test template processor
