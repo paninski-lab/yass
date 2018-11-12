small updates to cluster class; median-based tempalte computation; plotting function bug fixed (?); clipping waveforms to +/- 1000SU; renamed a few functions

src/yass/cluster/util.py
@@ -1985,6 +1985,7 @@ def get_connected_components(rhat, assignment):
 # def robust_stds(data):
     # return np.median(np.abs(data - np.median(data, axis=0, keepdims=True)), axis=0)*1.4826
     
+    
 # def get_feat_channels_mad4(wf, n_feat_chans):
     # '''  Function that uses MAD statistic like robust variance estimator to select channels
     # '''
@@ -2382,6 +2383,7 @@ def run_cluster_features_chunks(spike_index_clear, spike_index_all,
     # select which spike index to use:
     if True:
         print ("  using spike_index_all for clustering step")
+        print (spike_index_all.shape)
         spike_index = spike_index_all.copy()
     else:
         print ("  using spike_index_clear for clustering step")
@@ -2415,9 +2417,9 @@ def run_cluster_features_chunks(spike_index_clear, spike_index_all,
                                                     str(proc_index).zfill(6)
         channels = np.arange(CONFIG.recordings.n_channels)
         args_in = []
-        #for channel in channels:
-        for channel in [45]:
-        #for channel in [6,15,45,31,32]:
+        for channel in channels:
+        #for channel in [4,6,22,23]:
+        #for channel in [255]:
 
             # check to see if chunk + channel already completed
             filename_postclustering = (chunk_dir + "/channel_"+
@@ -2455,8 +2457,6 @@ def run_cluster_features_chunks(spike_index_clear, spike_index_all,
             res = []
             for arg_in in args_in:
                 res.append(Cluster(arg_in))
-
-        quit()
 
         ## save simple flag that chunk is done
         ## Cat: TODO: fix this; or run chunk wise-global merge

src/yass/deconvolve/run.py
@@ -118,7 +118,7 @@ def run(spike_train_cluster,
     
     # Cat: TODO: read both from CONFIG
     threshold = 10.    
-    conv_approx_rank = 15
+    conv_approx_rank = 10
     
     ''' 
     ***********************************************************
@@ -232,6 +232,14 @@ def run(spike_train_cluster,
     '''
     # run over rest of data in single chunk run:
     chunk_size = len(idx_list)
+
+    # process only white noise, first 30 mins
+    white_noise_only = True
+    if white_noise_only:
+        print ("  DECONV ONLY OVER WHITE NOISE STIMULUS ")
+        max_time = 30*60*CONFIG.recordings.sampling_rate
+        idx = np.where(np.array(idx_list)[:,0]<max_time)[0]
+        idx_list = idx_list[idx]
 
     #templates = templates.swapaxes(1,2)
     for chunk_ctr, c in enumerate(range(0, len(idx_list), chunk_size)):
@@ -691,9 +699,10 @@ def compute_residual_function(CONFIG, idx_list_local,
                                       deconv_chunk_dir,
                                       chunk_size,
                                       n_sec_chunk,
-                                      idx_list_local)
-    
-        
+                                      idx_list_local,
+                                      standardized_filename)
+
+
     # compute residual using initial templates obtained above
     # Note: this uses spike times occuring at beginning of spike
     fname = (deconv_chunk_dir+"/residual.npy")