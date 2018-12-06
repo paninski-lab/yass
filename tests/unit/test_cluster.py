84dd6ad705da94c3fa1e4f3cc18b2bd94ed54463
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-12-06 17:14:02 -0500

    refactors cluster tests

--- tests/unit/test_cluster.py
+++ tests/unit/test_cluster.py
@@ -5,41 +5,35 @@ some configurations
 import yass
 from yass import preprocess
 from yass import detect
-from yass.detect import threshold, nnet
 from yass import cluster
 
 
-def test_cluster_threshold(path_to_config, make_tmp_folder):
+def test_cluster_threshold(path_to_config_threshold, make_tmp_folder):
+    yass.set_config(path_to_config_threshold, make_tmp_folder)
+
+    (standarized_path,
+     standarized_params,
+     whiten_filter) = preprocess.run()
+
+    spike_index_all = detect.run(standarized_path,
+                                 standarized_params,
+                                 whiten_filter)
+
+    cluster.run(None, spike_index_all)
+
+
+def test_cluster_nnet(path_to_config, make_tmp_folder):
     yass.set_config(path_to_config, make_tmp_folder)
 
     (standarized_path,
      standarized_params,
      whiten_filter) = preprocess.run()
 
-    (spike_index_clear,
-     spike_index_all) = detect.run(standarized_path,
-                                   standarized_params,
-                                   whiten_filter,
-                                   function=threshold.run)
+    spike_index_all = detect.run(standarized_path,
+                                 standarized_params,
+                                 whiten_filter)
 
-    cluster.run(spike_index_clear)
-
-
-def test_cluster_nnet(patch_triage_network, path_to_config,
-                      make_tmp_folder):
-    yass.set_config(path_to_config, make_tmp_folder)
-
-    (standarized_path,
-     standarized_params,
-     whiten_filter) = preprocess.run()
-
-    (spike_index_clear,
-     spike_index_all) = detect.run(standarized_path,
-                                   standarized_params,
-                                   whiten_filter,
-                                   function=nnet.run)
-
-    cluster.run(spike_index_clear)
+    cluster.run(None, spike_index_all)
 
 
 def test_cluster_save_results(path_to_config, make_tmp_folder):
@@ -50,13 +44,11 @@ def test_cluster_save_results(path_to_config, make_tmp_folder):
      standarized_params,
      whiten_filter) = preprocess.run()
 
-    (spike_index_clear,
-     spike_index_all) = detect.run(standarized_path,
-                                   standarized_params,
-                                   whiten_filter,
-                                   function=threshold.run)
+    spike_index_all = detect.run(standarized_path,
+                                 standarized_params,
+                                 whiten_filter)
 
-    cluster.run(spike_index_clear, save_results=True)
+    cluster.run(None, spike_index_all, save_results=True)
 
 
 def test_cluster_loads_from_disk_if_all_files_exist(path_to_config,
@@ -67,18 +59,16 @@ def test_cluster_loads_from_disk_if_all_files_exist(path_to_config,
      standarized_params,
      whiten_filter) = preprocess.run()
 
-    (spike_index_clear,
-     spike_index_all) = detect.run(standarized_path,
-                                   standarized_params,
-                                   whiten_filter,
-                                   function=threshold.run)
+    spike_index_all = detect.run(standarized_path,
+                                 standarized_params,
+                                 whiten_filter)
 
     # save results
-    cluster.run(spike_index_clear, save_results=True)
+    cluster.run(None, spike_index_all, save_results=True)
 
     assert cluster.run.executed
 
     # next time this should not run and just load from files
-    cluster.run(spike_index_clear, save_results=True)
+    cluster.run(None, spike_index_all, save_results=True)
 
     assert not cluster.run.executed
