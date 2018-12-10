09469a93e6be0fde6dff4033e292302fee03c6a3
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-12-10 14:32:14 -0500

    fixes test_pipeline

--- tests/unit/test_pipeline.py
+++ tests/unit/test_pipeline.py
@@ -3,19 +3,16 @@ Testing pipeline.run
 """
 import yaml
 from yass import pipeline
-from yass.detect import nnet, threshold
 
 
 def test_works_with_nnet_config(patch_triage_network, path_to_config,
                                 make_tmp_folder):
-    pipeline.run(path_to_config, output_dir=make_tmp_folder,
-                 detector=nnet.run)
+    pipeline.run(path_to_config, output_dir=make_tmp_folder)
 
 
-def test_works_with_threshold_config(path_to_config,
+def test_works_with_threshold_config(path_to_config_threshold,
                                      make_tmp_folder):
-    pipeline.run(path_to_config, output_dir=make_tmp_folder,
-                 detector=threshold.run)
+    pipeline.run(path_to_config_threshold, output_dir=make_tmp_folder)
 
 
 def test_works_with_sample_config_passing_dict(path_to_config,
@@ -23,5 +20,4 @@ def test_works_with_sample_config_passing_dict(path_to_config,
     with open(path_to_config) as f:
         cfg = yaml.load(f)
 
-    pipeline.run(cfg, output_dir=make_tmp_folder,
-                 detector=threshold.run)
+    pipeline.run(cfg, output_dir=make_tmp_folder)
