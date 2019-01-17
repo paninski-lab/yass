9e85f8a935f79734ec74449e453cc447bb19390b
Peter Lee <jinhyungp.lee@gmail.com>
2019-01-17 01:40:14 -0500

    remove it ..

--- src/yass/cluster/plot.py
+++ src/yass/cluster/plot.py
@@ -4,7 +4,6 @@ from yass.util import absolute_path_to_asset
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
 
 
 def plot_normalized_templates(templates_dir, neigh_channels):
