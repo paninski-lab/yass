7dd8186b10c5072fea3f337f18ca4720cb15df9a
root <nd2506@columbia.edu>
2019-03-06 10:42:37 -0500

    added notch finder

--- src/yass/visual/util.py
+++ src/yass/visual/util.py
@@ -98,3 +98,58 @@ def combine_two_rf(STAs1, STAs2, Gaussian_params1, Gaussian_params2):
     
     return STAs, Gaussian_params
 
+
+def ttest_notch(greater, smaller, data, sig = 0.05):
+    N = data.sum()
+    
+    ngreater, nsmaller = data[greater], data[smaller]
+    
+    
+    pg = ngreater/N
+    pg = pg.sum()/pg.size
+    siggsq = pg*(1-pg)/N
+    siggsq = siggsq.sum()/siggsq.size**2    
+    
+    ps  = nsmaller/N
+    ps = ps.sum()/ps.size
+    siggsq = sigssq = ps*(1-ps)/N
+    sigssq = sigssq.sum()/sigssq.size**2
+    
+    tstat = (pg - ps)/np.sqrt(siggsq + sigssq)
+    df = (siggsq + sigssq)**2/((siggsq)**2/(N-1) + (sigssq)**2/(N-1) )
+    
+    if tstat > 0 and (1 - t.cdf(tstat, df)) < sig:
+        return True
+    else:
+        return False
+
+def findnotchpairs(correlograms, mc, CONFIG):
+    
+    nunits = correlograms.shape[0]
+    
+    baseline = np.arange(correlograms.shape[2]//2-29, correlograms.shape[2]//2-8)
+    baseline = np.concatenate([baseline, np.arange(correlograms.shape[2]//2+8, correlograms.shape[2]//2+28)])
+    centrebins = [correlograms.shape[2]//2]
+    offbins = np.arange(correlograms.shape[2]//2-7, correlograms.shape[2]//2-2)
+    offbins = np.concatenate([offbins, np.arange(correlograms.shape[2]//2+2, correlograms.shape[2]//2+7)])
+    
+    notchpairs = []
+    
+    
+    for unit1 in range(nunits):
+        idx = np.in1d(mc, np.arange(49)[CONFIG.neigh_channels[mc[unit1]]])
+        closeunits = np.where(idx2)[0]
+        notchpairs.append([])
+        for unit2 in closeunits:
+            if ttest_notch(baseline, centrebins, correlograms[unit1, unit2])[0]:
+                if ttest_notch(offbins, baseline, correlograms[unit1, unit2])[0]:
+                    notchpairs[unit1].append(unit2)
+                    
+    return notchpairs
+                   
+ 
+            
+        
+        
+    
+    
