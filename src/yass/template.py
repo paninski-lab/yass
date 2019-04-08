06e5998930fe2d80741506ecd4c0a08678bf05c3
jinhyunglee <jinhyungp.lee@gmail.com>
2019-04-08 03:36:35 -0400

    alignment fix

--- src/yass/template.py
+++ src/yass/template.py
@@ -2,9 +2,13 @@
 
 import numpy as np
 import math
+import logging
+
 from scipy.interpolate import interp1d
 from scipy.spatial.distance import pdist, squareform
 from scipy import signal
+
+from yass.util import absolute_path_to_asset
 
 class Geometry(object):
     """Geometry Object for finidng closest channels."""
@@ -183,6 +187,162 @@ class WaveForms(object):
                 all_shifts - ref).sum(axis=-1).argmin(axis=-1)
         return self.get_shifted_waveforms(best_shift_idx, clip_value=jitter)
 
+def run_template_computation(
+    fname_spike_train,
+    spike_size, reader, out_dir,
+    multi_processing=None,
+    n_processors=1):
+
+    logger = logging.getLogger(__name__)
+    
+    logger.info("computing templates")
+
+    # make output folder
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+        
+    # make temp folder
+    tmp_folder = os.path.join(out_dir, 'tmp_template')
+    if not os.path.exists(tmp_folder):
+        os.makedirs(tmp_folder)
+
+    # partition spike train per unit for multiprocessing
+    fnames_spike_times, n_units = partition_spike_time(
+        tmp_folder, fname_spike_train)
+
+    # gather input arguments
+    fnames_out = []
+    for unit in range(n_units):
+        fnames_out.append(os.path.join(
+            tmp_folder,
+            "template_unit_{}.npy".format(unit)))
+
+    # run computing function
+    if multi_processing:
+        parmap.starmap(run_template_computation_parallel,
+                   list(zip(fnames_spike_times, fnames_out)),
+                   reader,
+                   spike_size,
+                   processes=n_processors,
+                   pm_pbar=True)
+    else:
+        for ctr in range(n_units):
+            run_template_computation_parallel(
+                fnames_spike_times[ctr],
+                fnames_out[ctr],
+                reader,
+                spike_size)
+
+    # gather all info
+    templates_new = np.zeros((n_units, spike_size, n_channels))
+    for ctr, unit in enumerate(range(n_units)):
+        if os.path.exists(fnames_out[ctr]):
+            templates_new[unit] = np.load(fnames_out[ctr])
+
+    fname_templates = os.path.join(out_dir, 'templates.npy')
+    np.save(fname_templates, templates_new)
+
+    return fname_templates
+
+def run_template_computation_parallel(
+    fname_spike_times, fname_out, reader, spike_size):
+
+    # load spike times
+    spike_times = np.load(fname_spike_times)
+
+    template = compute_a_template(spike_times,
+                                  reader,
+                                  spike_size)
+
+    # save result
+    np.save(fname_out, template)
+
+def compute_a_template(spike_times, reader, spike_size):
+
+    # subsample upto 1000
+    max_spikes = 1000
+    if len(spike_times) > max_spikes:
+        spike_times = np.random.choice(a=spike_times,
+                                       size=max_spikes,
+                                       replace=False)
+
+    # get waveforms
+    wf, _ = reader.read_waveforms(spike_times,
+                                  spike_size)
+
+    # max channel
+    mc = np.mean(wf, axis=0).ptp(0).argmax()
+
+    # load reference template
+    ref_template = np.load(absolute_path_to_asset(
+            os.path.join('template_space', 'ref_template.npy')))
+
+
+    wf = align_waveforms(wf=wf,
+                         ref=ref_template,
+                         upsample_factor=5,
+                         nshifts=3)
+
+    return np.median(wf, axis=0)
+
+def partition_spike_time(save_dir,
+                         fname_spike_index):
+
+    # make directory
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+
+    # load data
+    spike_index = np.load(fname_spike_index)
+
+    # re-organize spike times and templates id
+    n_units = np.max(spike_index[:, 1]) + 1
+    spike_index_list = [[] for ii in range(n_units)]
+    for j in range(len(spike_index)):
+        tt, ii = spike_index[j]
+        spike_index_list[ii].append(tt)
+
+    # save them
+    fnames = []
+    for unit in range(n_units):
+
+        fname = os.path.join(save_dir, 'partition_{}.npy'.format(unit))
+        np.save(fname,
+                spike_index_list[unit])
+        fnames.append(fname)
+        
+    return fnames, n_units
+
+def align_waveforms(wf, ref=None, upsample_factor=5, nshifts=7):
+
+    # get shapes
+    n_spikes, n_times, n_channels = wf.shape
+
+    # if no reference, make a reference using mean
+    if ref is None:
+        mean_wf = np.mean(wf, axis=0)
+        mc = mean_wf.armgax(1)
+        ref = mean_wf[:, mc]
+
+    n_times_ref = len(ref)
+
+    # if length of waveforms is different than ref,
+    # either pad zeros to ref or cut edges of ref
+    len_diff = n_times - n_times_ref
+    if len_diff > 0:
+        ref = np.hstack((np.zeros(len_diff//2),
+                         ref,
+                         np.zeros(len_diff - len_diff//2)))
+    elif len_diff < 0:
+        ref = ref[len_diff//2:-(len_diff-len_diff//2)]
+    
+    shifts = align_get_shifts_with_ref(
+        wf, ref, upsample_factor, nshifts)
+    
+    wf_aligned = shift_chans(wf, shifts)
+    
+    return wf_aligned, shifts
+    
 def align_get_shifts_with_ref(wf, ref, upsample_factor = 5, nshifts = 7):
 
     ''' Align all waveforms on a single channel
