bug fix/refactor pd merge, incorporate noise soft assignment in pd merge, rf computation and vis package

src/yass/merge/correlograms_phy.py
@@ -71,12 +71,12 @@ def _as_array(arr, dtype=None):
     return out
 
 
-def _increment(arr, indices):
+def _increment(arr, indices, weight=None):
     """Increment some indices in a 1D vector of non-negative integers.
     Repeated indices are taken into account."""
     arr = _as_array(arr)
     indices = _as_array(indices)
-    bbins = np.bincount(indices)
+    bbins = np.bincount(indices, weight)
     arr[:len(bbins)] += bbins
     return arr
 
@@ -88,7 +88,7 @@ def _diff_shifted(arr, steps=1):
 
 def _create_correlograms_array(n_clusters, winsize_bins):
     return np.zeros((n_clusters, n_clusters, winsize_bins // 2 + 1),
-                    dtype=np.int32)
+                    'float32')
 
 
 def _symmetrize_correlograms(correlograms):
@@ -111,12 +111,13 @@ def _symmetrize_correlograms(correlograms):
 
 def correlograms(spike_times,
                  spike_clusters,
+                 soft_assignment,
                  cluster_ids=None,
                  sample_rate=1.,
                  bin_size=None,
                  window_size=None,
-                 symmetrize=True,
-                 ):
+                 symmetrize=True):
+
     """Compute all pairwise cross-correlograms among the clusters appearing
     in `spike_clusters`.
     Parameters
@@ -138,6 +139,7 @@ def correlograms(spike_times,
         A `(n_clusters, n_clusters, winsize_samples)` array with all pairwise
         CCGs.
     """
+
     assert sample_rate > 0.
     assert np.all(np.diff(spike_times) >= 0), ("The spike times must be "
                                                "increasing.")
@@ -145,7 +147,6 @@ def correlograms(spike_times,
     # Get the spike samples.
     spike_times = np.asarray(spike_times, dtype=np.float64)
     spike_samples = (spike_times * sample_rate).astype(np.int64)
-
     
     spike_clusters = _as_array(spike_clusters)
 
@@ -209,8 +210,11 @@ def correlograms(spike_times,
                                         d),
                                        correlograms.shape)
 
+        # weight
+        weight = soft_assignment[:-shift][m]*soft_assignment[+shift:][m]
+
         # Increment the matching spikes in the correlograms array.
-        _increment(correlograms.ravel(), indices)
+        _increment(correlograms.ravel(), indices, weight)
 
         shift += 1
 
@@ -225,21 +229,31 @@ def correlograms(spike_times,
         return correlograms
 
 
-def compute_correlogram(units, spike_train, sample_rate=20000, bin_width = 0.001, window_size = 0.1):
+def compute_correlogram(units, spike_train, soft_assignment=None, sample_rate=20000, bin_width = 0.001, window_size = 0.1):
 
     #Reduce spike_train to two units; ensure to keep order of spikes 
     #spike_train_temp = []
     #for unit in units:
     #    spike_train_temp.append(spike_train[np.where(spike_train[:,1]==unit)[0]])
     #spike_train = np.vstack(spike_train_temp)
-   
-    spike_train = spike_train[np.in1d(spike_train[:,1], units)]
     
+    if soft_assignment is None:
+        soft_assignment = np.ones(len(spike_train))
+
+    idx_in = np.where(np.in1d(spike_train[:,1], units))[0]
+    spike_train = spike_train[idx_in]
+    soft_assignment = soft_assignment[idx_in]
     
     order_indexes = np.argsort(spike_train[:,0])
     spike_train = spike_train[order_indexes]
+    soft_assignment = soft_assignment[order_indexes]
     
-    return correlograms(spike_train[:,0]/float(sample_rate),spike_train[:,1],sample_rate=sample_rate, bin_size=bin_width, window_size=window_size)
+    return correlograms(spike_train[:,0]/float(sample_rate), 
+                        spike_train[:,1],
+                        soft_assignment,
+                        sample_rate=sample_rate,
+                        bin_size=bin_width,
+                        window_size=window_size)
 
 
 def compute_correlogram_v2(spt1, spt2, sample_rate=20000, bin_width = 0.001, window_size = 0.1):

src/yass/merge/run.py
@@ -9,15 +9,11 @@ from yass.merge.util import (partition_input,
 from yass.merge.merge import TemplateMerge
 
 def run(output_directory,
-        raw_data,
         fname_spike_train,
         fname_templates,
-        fname_spike_train_up=None,
-        fname_templates_up=None,
-        fname_recording=None,
-        recording_dtype=None,
-        fname_residual=None,
-        residual_dtype=None):
+        fname_soft_assignment,
+        fname_residual,
+        residual_dtype):
 
     logger = logging.getLogger(__name__)
 
@@ -31,48 +27,39 @@ def run(output_directory,
                                          'spike_train.npy')
     fname_templates_out = os.path.join(output_directory,
                                        'templates.npy')
+    fname_soft_assignment_out = os.path.join(output_directory,
+                                       'soft_assignment.npy')
 
     if os.path.exists(fname_spike_train_out) and os.path.exists(fname_templates_out):
-        return fname_templates_out, fname_spike_train_out
+        return (fname_templates_out,
+                fname_spike_train_out,
+                fname_soft_assignment_out)
 
-    # partition spike_idnex_chunk using the second column
-    partition_dir = os.path.join(output_directory, 'input_partition')
-    logger.info("partinioiong input: ") 
-    fnames_input = partition_input(partition_dir,
-                                   fname_templates,
-                                   fname_spike_train,
-                                   fname_templates_up,
-                                   fname_spike_train_up)
-
-    # get reader
-    if raw_data:
-        reader = READER(fname_recording,
-                        recording_dtype,
-                        CONFIG)
-    else:
-        reader = READER(fname_residual,
-                        residual_dtype,
-                        CONFIG)
+    reader_residual = READER(fname_residual,
+                             residual_dtype,
+                             CONFIG)
 
     # initialize merge: find candidates
     logger.info("finding merge candidates")
     tm = TemplateMerge(
         output_directory,
-        raw_data,
-        reader,
+        reader_residual,
         fname_templates,
-        fnames_input,
+        fname_spike_train,
+        fname_soft_assignment,
         CONFIG.resources.multi_processing,
         CONFIG.resources.n_processors)
 
     # find merge pairs
     logger.info("merging pairs")
-    merge_pairs = tm.get_merge_pairs()
+    tm.get_merge_pairs()
 
     # update templates adn spike train accordingly
     logger.info("udpating templates and spike train")
-    spike_train_new, templates_new, merge_array = merge_units(
-        fname_templates, fname_spike_train, merge_pairs)
+    (templates_new,
+     spike_train_new,
+     soft_assignment_new,
+     merge_array) = tm.merge_units()
 
     # save results
     fname_merge_array = os.path.join(output_directory,
@@ -80,7 +67,10 @@ def run(output_directory,
     np.save(fname_merge_array, merge_array)    
     np.save(fname_spike_train_out, spike_train_new)
     np.save(fname_templates_out, templates_new)
+    np.save(fname_soft_assignment_out, soft_assignment_new)
 
     logger.info('Number of units after merge: {}'.format(templates_new.shape[0]))
 
-    return fname_templates_out, fname_spike_train_out
+    return (fname_templates_out,
+            fname_spike_train_out,
+            fname_soft_assignment_out)

src/yass/visual/run.py
@@ -69,7 +69,8 @@ class Visualizer(object):
     def __init__(self, fname_templates, fname_spike_train,
                  fname_recording, recording_dtype,
                  CONFIG, save_dir, rf_dir=None,
-                 fname_residual=None, residual_dtype=None):
+                 fname_residual=None, residual_dtype=None,
+                 fname_soft_assignment=None):
 
         # saving directory location
         self.save_dir = save_dir
@@ -96,6 +97,8 @@ class Visualizer(object):
         # load spike train and templates
         self.spike_train = np.load(fname_spike_train)
         self.unique_ids = np.unique(self.spike_train[:,1])
+        self.soft_assignment = np.load(fname_soft_assignment)
+
         # compute firing rates
         self.compute_firing_rates()
         self.compute_xcorrs()
@@ -118,25 +121,20 @@ class Visualizer(object):
             self.STAs = np.flip(self.STAs, axis=2)
             self.STAs_temporal = np.load(os.path.join(rf_dir, 'STA_temporal.npy'))
             self.gaussian_fits = np.load(os.path.join(rf_dir, 'gaussian_fits.npy'))
-            
-            self.idx_single_rf = np.load(os.path.join(rf_dir, 'idx_single_rf.npy'))
-            self.idx_no_rf = np.load(os.path.join(rf_dir, 'idx_no_rf.npy'))
-            self.idx_multi_rf = np.load(os.path.join(rf_dir, 'idx_multi_rf.npy'))
 
             self.cell_types = list(np.load(os.path.join(rf_dir, 'cell_types.npy')))
-            self.cell_types += ['Multiple-Rf', 'No-Rf']
+            self.cell_types += ['No-Rf', 'Multiple-Rf']
 
-            if os.path.exists(os.path.join(rf_dir, 'labels_new.npy')):
-                self.rf_labels = np.load(os.path.join(rf_dir, 'labels_new.npy'))
+            if os.path.exists(os.path.join(rf_dir, 'labels_updated.npy')):
+                self.rf_labels = np.load(os.path.join(rf_dir, 'labels_updated.npy'))
             else:
+                self.idx_single_rf = np.load(os.path.join(rf_dir, 'idx_single_rf.npy'))
+                self.idx_no_rf = np.load(os.path.join(rf_dir, 'idx_no_rf.npy'))
+                self.idx_multi_rf = np.load(os.path.join(rf_dir, 'idx_multi_rf.npy'))
+
                 self.rf_labels = np.load(os.path.join(rf_dir, 'labels.npy'))
-            self.idx_multi_rf = self.idx_multi_rf[
-                self.rf_labels[self.idx_multi_rf] == -1]
-            self.idx_no_rf = self.idx_no_rf[
-                self.rf_labels[self.idx_no_rf] == -1]
-            self.rf_labels[self.idx_multi_rf] = len(self.cell_types) - 2
-            self.rf_labels[self.idx_no_rf] = len(self.cell_types) - 1
-            self.rf_labels[self.rf_labels== -1] = len(self.cell_types) - 1
+                self.rf_labels[self.idx_multi_rf] = len(self.cell_types) - 1
+                self.rf_labels[self.idx_no_rf] = len(self.cell_types) - 2
 
             self.stim_size = np.load(os.path.join(rf_dir, 'stim_size.npy'))
 
@@ -179,11 +177,12 @@ class Visualizer(object):
         samplerate = self.sampling_rate
         rec_len = np.ptp(self.spike_train[:, 0])/samplerate
 
-        # compute firing rates and ptps
-        unique, n_spikes = np.unique(self.spike_train[:,1], return_counts=True)
-        
-        self.f_rates = np.zeros(self.n_units)
-        self.f_rates[unique] = n_spikes/rec_len
+        n_spikes_soft = np.zeros(self.n_units)
+        for j in range(self.spike_train.shape[0]):
+            n_spikes_soft[self.spike_train[j, 1]] += self.soft_assignment[j]
+        n_spikes_soft = n_spikes_soft.astype('int32')
+
+        self.f_rates = n_spikes_soft/rec_len
         self.ptps = self.templates.ptp(0).max(0)
 
     def compute_xcorrs(self):
@@ -197,6 +196,8 @@ class Visualizer(object):
             self.xcorrs = compute_correlogram(
                 np.arange(self.n_units),
                 self.spike_train,
+                self.soft_assignment,
+                sample_rate=self.sampling_rate,
                 bin_width=self.bin_width,
                 window_size=self.window_size)
             np.save(fname, self.xcorrs)
@@ -250,7 +251,7 @@ class Visualizer(object):
                         other_units = units_same_class[units_same_class != k]
                         nearest_units_rf[k][:len(other_units)] = other_units
                         nearest_units_rf[k][len(other_units):] = k
-                        
+
 
             self.nearest_units_rf = nearest_units_rf
             np.save(fname, self.nearest_units_rf)
@@ -393,18 +394,19 @@ class Visualizer(object):
                 self.make_neighbors_plot(unit, neighbor_units, fname, title)
 
             # xcorr neighbours
-            (idx_max, max_vals,
-             idx_min, min_vals) = self.compute_neighbours_xcorrs(unit)
-            fname = os.path.join(save_dir, name+'_p4_high_xcorr_neigh.png')
-            title = 'Unit {} ({}), Xcor Space Neighbors'.format(
-                unit, cell_type)
-            self.make_neighbors_plot(unit, idx_max, fname, title)
-
-            if np.min(min_vals) < -10:
-                fname = os.path.join(save_dir, name+'_p5_xcorr_notches.png')
-                title = 'Unit {} ({}), Xcor Notches'.format(
+            if self.f_rates[unit] > 0.5:
+                (idx_max, max_vals,
+                 idx_min, min_vals) = self.compute_neighbours_xcorrs(unit)
+                fname = os.path.join(save_dir, name+'_p4_high_xcorr_neigh.png')
+                title = 'Unit {} ({}), Xcor Space Neighbors'.format(
                     unit, cell_type)
-                self.make_neighbors_plot(unit, idx_min, fname, title)
+                self.make_neighbors_plot(unit, idx_max, fname, title)
+
+                if np.min(min_vals) < -10:
+                    fname = os.path.join(save_dir, name+'_p5_xcorr_notches.png')
+                    title = 'Unit {} ({}), Xcor Notches'.format(
+                        unit, cell_type)
+                    self.make_neighbors_plot(unit, idx_min, fname, title)
 
 
     def make_template_plot(self, unit, fname):
@@ -756,10 +758,16 @@ class Visualizer(object):
     def get_waveforms(self, unit, n_examples=200):
 
         idx = np.where(self.spike_train[:,1]==unit)[0]
-        idx = np.random.choice(idx, 
-                               np.min((n_examples, len(idx))),
-                               False)
         spt = self.spike_train[idx, 0]
+        prob = self.soft_assignment[idx]
+        
+        if np.sum(prob) < 1:
+            return np.zeros((0,1,1)), None, None, None
+
+        spt = np.random.choice(spt, 
+                               np.min((n_examples, int(np.sum(prob)))),
+                               False, prob/np.sum(prob))
+
         mc = self.templates[:, :, unit].ptp(0).argmax()
         neigh_chans = np.where(self.neigh_channels[mc])[0]
         temp = self.templates[:, :, unit][:, mc]
@@ -1196,6 +1204,7 @@ class Visualizer(object):
         l2_features, spike_ids = get_l2_features(
             self.reader_resid, self.spike_train,
             self.templates.transpose(2,0,1),
+            self.soft_assignment,
             unit1, unit2)
 
         if l2_features is None:
@@ -1338,7 +1347,11 @@ class Visualizer(object):
             k = order[j]
             idx = self.spike_train[:,1] == k
             spt = self.spike_train[idx, 0]/self.sampling_rate
-            plt.eventplot(spt, lineoffsets=j, color='k', linewidths=0.01)
+            prob = self.soft_assignment[idx]
+            if np.sum(prob) > 1:
+                spt = np.sort(np.random.choice(
+                    spt, int(np.sum(prob)), False, prob/np.sum(prob)))
+                plt.eventplot(spt, lineoffsets=j, color='k', linewidths=0.01)
         plt.yticks(np.arange(0,self.n_units,10), sorted_ptps[0:self.n_units:10])
         plt.ylabel('ptps', fontsize=self.fontsize)
         plt.xlabel('time (seconds)', fontsize=self.fontsize)