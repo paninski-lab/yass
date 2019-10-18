dbda9e2a4060512d05fb3f28f2ce20c55ff7d9de
jinhyunglee <jinhyungp.lee@gmail.com>
2019-10-18 17:25:50 -0400

    update pd merge using whitened data

--- src/yass/merge/run.py
+++ src/yass/merge/run.py
@@ -1,15 +1,18 @@
 import os
 import logging
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
 from yass import read_config
 from yass.reader import READER
 from yass.merge.util import (partition_input,
                              merge_units)
 from yass.merge.merge import TemplateMerge
+from yass.augment.noise import kill_signal, search_noise_snippets
 
 def run(output_directory,
         fname_spike_train,
+        fname_shifts,
         fname_templates,
         fname_soft_assignment,
         fname_residual,
@@ -39,6 +42,14 @@ def run(output_directory,
                              residual_dtype,
                              CONFIG)
 
+    # get whitening filters
+    fname_spatial_cov = os.path.join(output_directory, 'spatial_cov.npy')
+    fname_temporal_cov = os.path.join(output_directory, 'temporal_cov.npy')
+    if not (os.path.exists(fname_spatial_cov) and os.path.exists(fname_temporal_cov)):
+        spatial_cov, temporal_cov = get_covariance(reader_residual, CONFIG)
+        np.save(fname_spatial_cov, spatial_cov)
+        np.save(fname_temporal_cov, temporal_cov)
+
     # initialize merge: find candidates
     logger.info("finding merge candidates")
     tm = TemplateMerge(
@@ -46,7 +57,11 @@ def run(output_directory,
         reader_residual,
         fname_templates,
         fname_spike_train,
+        fname_shifts,
         fname_soft_assignment,
+        fname_spatial_cov,
+        fname_temporal_cov,
+        CONFIG.geom,
         CONFIG.resources.multi_processing,
         CONFIG.resources.n_processors)
 
@@ -74,3 +89,51 @@ def run(output_directory,
     return (fname_templates_out,
             fname_spike_train_out,
             fname_soft_assignment_out)
+
+
+def get_covariance(reader, CONFIG):
+    
+    # get data chunk
+    chunk_5sec = 5*CONFIG.recordings.sampling_rate
+    if CONFIG.rec_len < chunk_5sec:
+        chunk_5sec = CONFIG.rec_len
+    small_batch = reader.read_data(
+    data_start=CONFIG.rec_len//2 - chunk_5sec//2,
+    data_end=CONFIG.rec_len//2 + chunk_5sec//2)
+    
+    # get noise floor of recording
+    noised_killed, is_noise_idx = kill_signal(small_batch, 3, CONFIG.spike_size)
+    
+    # spatial covariance
+    spatial_cov_all = np.divide(np.matmul(noised_killed.T, noised_killed),
+                        np.matmul(is_noise_idx.T, is_noise_idx))
+    sig = np.sqrt(np.diag(spatial_cov_all))
+    spatial_cov_all = spatial_cov_all/(sig[:,None]*sig[None])
+
+    chan_dist = squareform(pdist(CONFIG.geom))
+    chan_dist_unique = np.unique(chan_dist)
+    cov_by_dist = np.zeros(len(chan_dist_unique))
+    for ii, d in enumerate(chan_dist_unique):
+        cov_by_dist[ii] = np.mean(spatial_cov_all[chan_dist == d])
+    dist_in = cov_by_dist > 0.1
+    chan_dist_unique = chan_dist_unique[dist_in]
+    cov_by_dist = cov_by_dist[dist_in]
+    spatial_cov = np.vstack((cov_by_dist, chan_dist_unique)).T
+
+    # get noise snippets
+    noise_wf = search_noise_snippets(
+    noised_killed, is_noise_idx, 1000,
+    CONFIG.spike_size,
+    channel_choices=None,
+    max_trials_per_sample=100,
+    allow_smaller_sample_size=True)
+
+    # get temporal covariance
+    temp_cov = np.cov(noise_wf.T)
+    sig = np.sqrt(np.diag(temp_cov))
+    temp_cov = temp_cov/(sig[:,None]*sig[None])
+    
+    #w, v = np.linalg.eig(temp_cov)
+    #inv_half_cov = np.matmul(np.matmul(v, np.diag(1/np.sqrt(w))), v.T)
+    
+    return spatial_cov, temp_cov
