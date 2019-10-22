ff1141f78d8ec287ff1dcd4db9a242324a02966e
jinhyunglee <jinhyungp.lee@gmail.com>
2019-10-22 16:29:53 -0400

    turn off height fit until done

--- src/yass/deconvolve/run.py
+++ src/yass/deconvolve/run.py
@@ -199,7 +199,7 @@ def deconv_ONgpu2(fname_templates_in,
     d_gpu.RANK = 10
     d_gpu.vis_chan_thresh = 1.0
 
-    d_gpu.fit_height = True
+    d_gpu.fit_height = False
     d_gpu.height_penalty = 10000
     d_gpu.max_diff = 0.1
 
