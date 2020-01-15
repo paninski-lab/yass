fix broken template update for new deconv, fix bugs in temp temp computation, add offset to spike times immediately instead of doing it at the end

src/yass/deconvolve/match_pursuit_gpu.py
@@ -120,7 +120,7 @@ class deconvGPU(object):
             os.mkdir(self.temps_dir)
 
         # always copy the startng templates to initalize the process
-        fname_out_temporary = os.path.join(self.temps_dir, 'templates_in.npy')
+        fname_out_temporary = os.path.join(self.temps_dir, 'templates_init_in.npy')
         if os.path.exists(fname_out_temporary)==False:
             temps_temporary = np.load(fname_templates)
             np.save(fname_out_temporary, temps_temporary)
@@ -141,7 +141,7 @@ class deconvGPU(object):
         self.out_dir = out_dir
 
         #
-        self.fname_templates_in = fname_templates
+        #self.fname_templates_in = fname_templates
 
         # load geometry
         self.geom = np.loadtxt(os.path.join(CONFIG.data.root_folder, CONFIG.data.geometry))
@@ -200,6 +200,7 @@ class deconvGPU(object):
 
         temp_temp_fname = os.path.join(self.svd_dir,'temp_temp_sparse_svd_'+\
                 str((self.chunk_id+1)*self.CONFIG.resources.n_sec_chunk_gpu_deconv) + '.npy')
+
         # pad len is constant and is 1.5 ms on each side, i.e. a total of 3 ms
         pad_len = int(1.5 * self.CONFIG.recordings.sampling_rate / 1000.)
         # jitter_len is selected in a way that deconv works with 3 ms signals
@@ -217,9 +218,9 @@ class deconvGPU(object):
         #np.save(os.path.join(self.svd_dir, 'temp_norms.npy'), self.ttc.temp_norms)
         #np.save(os.path.join(self.svd_dir, 'unit_overlap.npy'), self.ttc.unit_overlap)
 
-        # save updated templates
+        # update self.temps to the denoised templates
         self.temps = self.ttc.residual_temps.transpose(1, 2, 0)
-        np.save(self.fname_templates, self.temps.transpose(2, 1, 0))
+        #np.save(self.fname_templates, self.temps.transpose(2, 1, 0))
 
         if self.update_templates:
             self.get_parameters_for_template_update()
@@ -280,7 +281,7 @@ class deconvGPU(object):
         #self.fname_templates = fname_templates
 
         # rest lists for each segment of time
-        self.offset_array = []
+        #self.offset_array = []
         self.spike_array = []
         self.neuron_array = []
         self.shift_list = []
@@ -306,6 +307,31 @@ class deconvGPU(object):
 
         # empty cache
         torch.cuda.empty_cache()
+
+        # gather results (and move to cpu)
+        self.gather_results()
+
+    def gather_results(self):
+
+        # make spike train
+        # get all spike times and neuron ids
+        spike_times = torch.cat(self.spike_array)
+        neuron_ids = torch.cat(self.neuron_array)
+        spike_train = torch.stack((spike_times, neuron_ids), axis=1).cpu().numpy()
+
+        # fix spike times
+        spike_train[:,0] = spike_train[:,0] + self.STIME//2 - (2 * self.jitter_diff)
+        self.spike_train = self.ttc.adjust_peak_times_for_residual_computation(spike_train)
+
+        # make shifts and heights
+        self.shifts = torch.cat(self.shift_list).cpu().numpy()
+        self.heights = torch.cat(self.height_list).cpu().numpy()
+
+        self.spike_array = None
+        self.neuron_array = None
+        self.shift_list = None
+        self.height_list = None
+
 
     def initialize_cpp(self):
 
@@ -682,7 +708,7 @@ class deconvGPU(object):
         #obj_function = np.zeros([NUNIT, data.shape[1] + 61 - 1])
         self.obj_gpu = torch.zeros((self.K, self.data.shape[1]+self.STIME-1 + 2 * self.jitter_diff),
                                     dtype=torch.float).cuda()
-                                    
+
         spat_comp_gpu = torch.from_numpy(self.ttc.spat_comp.transpose([0,2,1])).float().cuda()
         #print ("self.temp_comp: ", self.temp_comp.shape)
         # transfer temp_comp and reverse the time 
@@ -736,7 +762,7 @@ class deconvGPU(object):
 
     def save_spikes(self):
         # # save offset of chunk time; spiketimes and neuron ids
-        self.offset_array.append(self.offset)
+        #self.offset_array.append(self.offset)
         self.spike_array.append(self.spike_times[:,0])
         self.neuron_array.append(self.neuron_ids[:,0])
         self.shift_list.append(self.xshifts)
@@ -1366,8 +1392,8 @@ class deconvGPU(object):
     def compute_min_max_vals(self):
 
         # get all spike times and neuron ids
-        spike_times = torch.cat(self.spike_array)
-        neuron_ids = torch.cat(self.neuron_array)
+        spike_times = torch.from_numpy(self.spike_train[:, 0]).long().cuda()
+        neuron_ids = torch.from_numpy(self.spike_train[:, 1]).long().cuda()
 
         # get min/max values
         max_spikes = 10000
@@ -1379,7 +1405,7 @@ class deconvGPU(object):
             ii_end = counter[j+1]
             tt = spike_times[ii_start:ii_end, None] + torch.arange(-2, 3).cuda()
             min_max_loc_spikes = (self.min_max_loc[neuron_ids[ii_start:ii_end]][:, :, :, None] + 
-                                  tt[:, None, None] - self.STIME + 1)
+                                  tt[:, None, None] - self.STIME//2)
             chan_loc_spikes = (torch.arange(self.N_CHAN).cuda()[None, None, :, None].repeat(
                 min_max_loc_spikes.shape[0], 2, 1, 5))
             min_max_vals_spikes[ii_start:ii_end] = self.data[chan_loc_spikes, min_max_loc_spikes]

src/yass/deconvolve/run.py
@@ -303,12 +303,10 @@ def deconv_ONgpu2(fname_templates_in,
     for chunk_id in tqdm(range(reader.n_batches)):
         time_index = (chunk_id+1)*CONFIG.resources.n_sec_chunk_gpu_deconv
         fname = os.path.join(d_gpu.seg_dir,str(time_index).zfill(6)+'.npz')
-        spike_array = np.load(fname, allow_pickle=True)['spike_array']
-        for tmp in spike_array:
-            n_spikes += len(tmp)
+        n_spikes += len(np.load(fname, allow_pickle=True)['spike_train'])
 
     # loop over chunks and add spikes;
-    spike_train = np.zeros((n_spikes, 2),'int32')
+    spike_train = np.zeros((n_spikes, 2), 'int32')
     shifts = np.zeros(n_spikes, 'float32')
     scales = np.zeros(n_spikes, 'float32')
 
@@ -319,46 +317,30 @@ def deconv_ONgpu2(fname_templates_in,
         fname = os.path.join(d_gpu.seg_dir,str(time_index).zfill(6)+'.npz')
         data = np.load(fname, allow_pickle=True)
 
-        spike_array = data['spike_array']
-        neuron_array = data['neuron_array']
-        offset_array = data['offset_array']
-        shift_list = data['shift_list']
-        scale_list = data['height_list']
-        for p in range(len(spike_array)):
-            spike_times = spike_array[p].cpu().data.numpy()
-            idx_keep = np.logical_and(spike_times >= buffer_size,
-                                      spike_times < batch_size+buffer_size)
-            idx_keep = np.where(idx_keep)[0]
-            temp=np.zeros((len(idx_keep),2), 'int32')
-            temp[:,0]=spike_times[idx_keep]+offset_array[p]
-            temp[:,1]=neuron_array[p].cpu().data.numpy()[idx_keep]
+        offset = data['offset']
+        spike_train_chunk = data['spike_train']
+        shifts_chunk = data['shifts']
+        scales_chunk = data['heights']
 
-            # Cat: TODO: is it faster to make list and then array?
-            #            or make array on the fly?
-            #spike_train.extend(temp)
-            #shifts.append(shift_list[p].cpu().data.numpy()[idx_keep])
-            #scales.append(scale_list[p].cpu().data.numpy()[idx_keep])
-            idx = slice(counter, counter+len(idx_keep))
-            spike_train[idx] = temp
-            shifts[idx] = shift_list[p].cpu().data.numpy()[idx_keep]
-            scales[idx] = scale_list[p].cpu().data.numpy()[idx_keep]
+        idx_keep = np.logical_and(
+            spike_train_chunk[:, 0] >= buffer_size,
+            spike_train_chunk[:, 0] < batch_size + buffer_size)
+        idx_keep = np.where(idx_keep)[0]
 
-            counter += len(idx_keep)
+        # add offset
+        spike_train_chunk[:, 0] += offset
+
+        # stack data
+        idx = slice(counter, counter+len(idx_keep))
+        spike_train[idx] = spike_train_chunk[idx_keep]
+        shifts[idx] = shifts_chunk[idx_keep]
+        scales[idx] = scales_chunk[idx_keep]
+
+        counter += len(idx_keep)
 
     spike_train = spike_train[:counter]
     shifts = shifts[:counter]
     scales = scales[:counter]
-
-    # Cat; TODO: sepped this up.
-    print ("   vstacking spikes (TODO initalize large array and then try to fill it...): ")
-    #spike_train = np.vstack(spike_train)
-    #shifts = np.hstack(shifts)
-    #scales = np.hstack(scales)
-
-    # add half the spike time back in to get to centre of spike
-    spike_train[:,0] = spike_train[:,0]+temporal_size//2 - (2 * d_gpu.jitter_diff)
-    spike_train = d_gpu.ttc.adjust_peak_times_for_residual_computation(spike_train)
-
 
     # sort spike train by time
     print ("   ordering spikes: ")
@@ -367,14 +349,14 @@ def deconv_ONgpu2(fname_templates_in,
     shifts = shifts[idx]
     scales = scales[idx]
 
-    np.save(os.path.join(d_gpu.out_dir,
+    # remove duplicates
+    # Cat: TODO: are there still duplicates in spike trains!?
+    #print ("  skipping spike deduplication step ")
+    if False:
+        np.save(os.path.join(d_gpu.out_dir,
                          'spike_train_prededuplication.npy'),
             spike_train)
 
-    # remove duplicates
-    # Cat: TODO: are there still duplicates in spike trains!?
-    print ("  skipping spike deduplication step ")
-    if False:
         print ("removing duplicates... (TODO: remove this requirement eventually...)")
         for k in np.unique(spike_train[:,1]):
             idx = np.where(spike_train[:,1]==k)[0]
@@ -384,11 +366,11 @@ def deconv_ONgpu2(fname_templates_in,
                 print ("unit: ", k, " has duplicates: ", idx3.shape[0])
                 spike_train[idx[idx3],0]=-1E6
         
-    # quit()
-    idx = np.where(spike_train[:,0]==-1E6)[0]
-    spike_train = np.delete(spike_train, idx, 0)
-    shifts = np.delete(shifts, idx, 0)
-    scales = np.delete(scales, idx, 0)
+        # quit()
+        idx = np.where(spike_train[:,0]==-1E6)[0]
+        spike_train = np.delete(spike_train, idx, 0)
+        shifts = np.delete(shifts, idx, 0)
+        scales = np.delete(scales, idx, 0)
 
     # save spike train
     print ("  saving spike_train: ", spike_train.shape)
@@ -449,11 +431,10 @@ def run_deconv_no_templates_update_parallel(d_gpu, chunk_ids, n_sec_chunk_gpu, d
 
             # save deconv results
             np.savez(fname,
-                     spike_array = d_gpu.spike_array,
-                     offset_array = d_gpu.offset_array,
-                     neuron_array = d_gpu.neuron_array,
-                     shift_list = d_gpu.shift_list,
-                     height_list = d_gpu.height_list
+                     spike_train = d_gpu.spike_train,
+                     offset = d_gpu.offset,
+                     shifts = d_gpu.shifts,
+                     heights = d_gpu.heights
                     )
 
 def run_deconv_with_templates_update2(d_gpu):
@@ -464,6 +445,11 @@ def run_deconv_with_templates_update2(d_gpu):
                              n_chunks_update), d_gpu.reader.n_batches))
 
     d_gpu.initialize()
+    fname_templates_denoised = os.path.join(
+        d_gpu.out_dir,
+        'template_updates',
+        'templates_init.npy')
+    np.save(fname_templates_denoised, d_gpu.temps.transpose(2, 1, 0))
 
     for batch_id in range(len(update_chunk)-1):
 
@@ -493,14 +479,12 @@ def run_deconv_with_templates_update2(d_gpu):
 
                 # save deconv results
                 np.savez(fname,
-                         spike_array = d_gpu.spike_array,
-                         offset_array = d_gpu.offset_array,
-                         neuron_array = d_gpu.neuron_array,
-                         shift_list = d_gpu.shift_list,
-                         height_list = d_gpu.height_list,
-                         min_max_vals_average = min_max_vals_average.cpu(),
-                         weights = weights.cpu()
-                        )
+                         spike_train = d_gpu.spike_train,
+                         offset = d_gpu.offset,
+                         shifts = d_gpu.shifts,
+                         heights = d_gpu.heights,
+                         min_max_vals_average = min_max_vals_average.cpu().numpy(),
+                         weights = weights.cpu().numpy())
 
             else:
                 d_gpu.chunk_id = chunk_id
@@ -526,22 +510,28 @@ def run_deconv_with_templates_update2(d_gpu):
                     fname_templates_updated = os.path.join(
                         d_gpu.out_dir,
                         'template_updates',
-                        'templates_{}sec.npy'.format(n_chunks_update*n_sec_chunk*batch_id))
+                        'templates_in_{}sec.npy'.format(n_chunks_update*n_sec_chunk*batch_id))
                     fname_temp_min_loc = os.path.join(d_gpu.out_dir, 'templates_min_locs.npy')
                     update_templates(fnames_forward,
-                                     d_gpu.fname_templates,
+                                     fname_templates_denoised,
                                      fname_templates_updated,
                                      fname_temp_min_loc,
                                      update_weight = 50)
 
                     # re initialize with updated templates
                     d_gpu.fname_templates = fname_templates_updated
+
                     # make sure that it is at the right chunk location
                     d_gpu.chunk_id = chunk_id
                     d_gpu.initialize()
+
                     # resaving the templates that is processed 
                     # and actually used for deconv
-                    np.save(fname_templates_updated, d_gpu.temps.transpose(2, 1, 0))
+                    fname_templates_denoised = os.path.join(
+                        d_gpu.out_dir,
+                        'template_updates',
+                        'templates_{}sec.npy'.format(n_chunks_update*n_sec_chunk*batch_id))
+                    np.save(fname_templates_denoised, d_gpu.temps.transpose(2, 1, 0))
 
                     templates_reinitialized = True
 
@@ -553,11 +543,10 @@ def run_deconv_with_templates_update2(d_gpu):
 
                 # save deconv results
                 np.savez(fname,
-                         spike_array = d_gpu.spike_array,
-                         offset_array = d_gpu.offset_array,
-                         neuron_array = d_gpu.neuron_array,
-                         shift_list = d_gpu.shift_list,
-                         height_list = d_gpu.height_list)
+                         spike_train = d_gpu.spike_train,
+                         offset = d_gpu.offset,
+                         shifts = d_gpu.shifts,
+                         heights = d_gpu.heights)
 
             else:
                 d_gpu.chunk_id = chunk_id
@@ -633,6 +622,9 @@ def update_templates(fnames_forward,
                      fname_templates_updated,
                      fname_temp_min_loc,
                      update_weight = 50):
+
+    if os.path.exists(fname_templates_updated):
+        return
 
     n_chunks = len(fnames_forward)
 

src/yass/deconvolve/utils.py
@@ -482,6 +482,10 @@ class TempTempConv(object):
 
         # visible channels for the purpose of finiding overlapping units
         viscs = temp.ptp(2) > vis_threshold_strong
+        num_vis_chan = viscs.sum(1)
+        # If a unit has no visible channel, make its main channel visible
+        invis_units = np.where(num_vis_chan == 0)[0]
+        viscs[invis_units, temp.ptp(2).argmax(1)[invis_units]] = True
         unit_unit_overlap = np.logical_and(viscs[None], viscs[:, None]).sum(-1) > 0
 
         # real visible channels using both strong and weak threshold
@@ -616,16 +620,16 @@ class TempTempConv(object):
         # let's make the templates the same size as the input templates.
         min_loc = residual_computation_templates[max_ptp_unit, max_ptp_unit_main_chan].argmin()
         cut_off_begin = min_loc - min_loc_orig
-        if residual_computation_templates.shape[2] < n_time:
-            left_ = - min(cut_off_begin, 0)
-            cut_off_begin = 0
-            right_ = max(0, cut_off_begin + n_time - residual_computation_templates.shape[2] + 1)
+        if cut_off_begin < 0:
+            left_ = -np.copy(cut_off_begin)
+            right_ = max(0, n_time - left_ - residual_computation_templates.shape[2])
             residual_computation_templates = np.pad(
                 residual_computation_templates, ((0, 0), (0, 0), (left_, right_)), 'constant')
+            cut_off_begin = 0
         self.residual_temps = residual_computation_templates[:, :, cut_off_begin:cut_off_begin+n_time] + 0.
         # This needs to be applied to every peak time
         self.peak_time_residual_offset = - temp_size + 1 - main_chan_shift
-        self.peak_time_residual_offset += cut_off_begin
+        self.peak_time_residual_offset += (min_loc - min_loc_orig)
 
         # reduce the amount of overlapping units
         less_overlapping_units = True
@@ -633,7 +637,7 @@ class TempTempConv(object):
         if less_overlapping_units:
             threshold = 0.05*self.temp_norms
             threshold[threshold > 50] = 50
-            unit_unit_overlap = np.abs(zero_padded_temp_temp).max(2) > threshold
+            unit_unit_overlap = np.abs(zero_padded_temp_temp).max(2) > threshold[None]
 
         # What the user needs from this class
 